5e01a75a8e400055e07f474f33e819c0fd277781
kug3lblitz <sabbir0ahmed0@gmail.com>
2016-03-05 18:14:39 -0500

    begin prototyping

---
+++ proj/proj1/testbench.v
@@ -0,0 +1,34 @@
+module testbench();
+
+
+   reg [2:0] switches;
+
+   // 3 inputs for A, B, and C                                             
+   wire [1:0] out;
+
+   ledSegs proj1(out[1],out[0],switches[2],switches[1],switches[0]);
+
+
+   initial
+
+     begin
+        switches = 3'b111;
+
+        $display("\t\t switches=ABC, out\n");
+
+        #45 $finish;
+
+     end
+
+   always
+
+     begin
+        #5 switches = switches + 3'b001;
+
+        $monitor($time, " switches=%b, out=%b", switches, out);
+
+        // $monitor displays the time and the string everytime             
+        // 'switches' or 'out' changes value.                              
+     end
+
+endmodule
