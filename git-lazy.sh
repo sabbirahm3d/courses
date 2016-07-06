dc73b4c9a1e28d0ab7b4243610ef685c40a17d83
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-07-06 18:00:24 -0400

    project integrated

--- git-lazy.sh
+++ git-lazy.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 cd DS-DC-13
-echo "Attempting git pull..."
+echo "Synchronizing class repo..."
 
 if git pull | grep 'Already up-to-date.'; then
    true
-
 else
     cd ..
     cp -r -n DS-DC-13/* copy/
     git add -A
     git status
+fi
 
+cd ../Heat-Replay
+echo "Synchronizing project..."
+
+if git pull | grep 'Already up-to-date.'; then
+   true
+else
+    cd ..
+    git add -A
+    git status
 fi
