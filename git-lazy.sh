40634933617c6a4bf57e77a64bc2a7dca1391aac
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-07-09 19:09:24 -0400

    lets get this done

--- git-lazy.sh
+++ git-lazy.sh
@@ -5,20 +5,22 @@ echo "Synchronizing class repo..."
 
 if git pull | grep 'Already up-to-date.'; then
    true
-else
-    cd ..
-    cp -r -n DS-DC-13/* copy/
-    git add -A
-    git status
 fi
+# else
+cd ..
+cp -r -n DS-DC-13/* copy/
+git add -A
+cd Heat-Replay
+# fi
 
-cd ../Heat-Replay
+# cd ../Heat-Replay
 echo "Synchronizing project..."
 
 if git pull | grep 'Already up-to-date.'; then
    true
-else
-    cd ..
-    git add -A
-    git status
 fi
+# else
+cd ..
+git add -A
+git status
+# fi
