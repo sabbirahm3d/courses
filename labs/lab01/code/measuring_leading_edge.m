ad5cf0b45f0bcbada9c8529f0d855f6db7da5075
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-09-16 00:19:56 -0400

    actually finished all the scripts, time to compile replrt

--- labs/lab01/code/measuring_leading_edge.m
+++ labs/lab01/code/measuring_leading_edge.m
@@ -1,4 +1,4 @@
-rand_ints = round(rand(10,1), 1);
+rand_ints = round(rand(10000,1), 1);
 
 t = -1:(1/10):4;
 T = 1;
@@ -10,8 +10,45 @@ first_non_zero = @(t, p) (t(find(p, 1)));  % first non-zero sample
 rand_pulses = zeros(size(rand_ints));
 
 for index = 1 : numel(rand_ints)
-    disp(index)
-    rand_pulses(index) = pulse(t-rand_ints(index), T);
+    rand_pulses(index) = first_non_zero(t, pulse(t-rand_ints(index), T));
 end
 
-rand_pulses
+Count = length(rand_pulses);
+Minimum = min(rand_pulses);
+Maximum = max(rand_pulses);
+Mean = mean(rand_pulses);
+Standard_Deviation = std(rand_pulses);
+
+stats = table(Count, Minimum, Maximum, Mean, Standard_Deviation);
+
+writetable(stats, 'stats.csv');
+
+
+
+rand_ints = round(rand(10000,1), 2);
+
+t = -1:0.05:4;
+T = 1;
+
+u = @(t) (t >= 0);  % unit step
+pulse = @(t, T) (u(t)) - (u(t-T));  % pulse
+first_non_zero = @(t, p) (t(find(p, 1)));  % first non-zero sample
+
+rand_pulses = zeros(size(rand_ints));
+
+for index = 1 : numel(rand_ints)
+    rand_pulses(index) = first_non_zero(t, pulse(t-rand_ints(index), T));
+end
+
+Count = length(rand_pulses);
+Minimum = min(rand_pulses);
+Maximum = max(rand_pulses);
+Mean = mean(rand_pulses);
+Standard_Deviation = std(rand_pulses);
+
+stats = table(Count, Minimum, Maximum, Mean, Standard_Deviation);
+
+writetable(stats, 'stats_1.csv');
+
+
+
