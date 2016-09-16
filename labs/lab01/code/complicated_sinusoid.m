fb2549a4884a6300e319b58d6af6920f7982196b
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-09-15 22:36:13 -0400

    sabbir pls

--- labs/lab01/code/complicated_sinusoid.m
+++ labs/lab01/code/complicated_sinusoid.m
@@ -4,8 +4,8 @@ dsb_am = 1 + 0.25*sin(180*pi*t)+0.15*sin(300*pi*t)+0.4*sin(2040*pi*t);
 figure;
 plot(t, dsb_am)
 title('x(t) = 1 + 0.25sin(180\pit) + 0.15sin(300\pit) + 0.4sin(2040\pit)')
-xlabel('Units')
-ylabel('Time (s)')
+xlabel('Time (s)')
+ylabel('Units')
 
 t = -0.1:(1/(2*1020)):0.1;
 dsb_am = 1 + 0.25*sin(180*pi*t)+0.15*sin(300*pi*t)+0.4*sin(2040*pi*t);
@@ -13,5 +13,5 @@ dsb_am = 1 + 0.25*sin(180*pi*t)+0.15*sin(300*pi*t)+0.4*sin(2040*pi*t);
 figure;
 plot(t, dsb_am)
 title('x(t) = 1 + 0.25sin(180\pit) + 0.15sin(300\pit) + 0.4sin(2040\pit)')
-xlabel('Units')
-ylabel('Time (s)')
+xlabel('Time (s)')
+ylabel('Units')
