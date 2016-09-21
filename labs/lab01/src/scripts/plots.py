c992638a565e96c19d48b66acefa11834d3bbf83
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-09-21 16:27:06 -0400

    add ideal plot figures

--- labs/lab01/src/scripts/plots.py
+++ labs/lab01/src/scripts/plots.py
@@ -38,34 +38,41 @@ def plot_ideal_IV(title, fig_name, piecewise=False):
     plt.xlabel('Diode Voltage (V)')
 
     if piecewise:
+
         CUTOFF_VOLTAGE = 1.2  # datasheet of 1N4732
         x = arange(-10.0, 10.0, 0.01)
+
         x[len(x) / 2 + 1] = 0.0
-        print flatnonzero(x == 0.0)[0], len(x) / 2 + 1, x[len(x) / 2 + 1]
+
         y = [0.0] * (len(x) / 2 + 1)
-        print y
-        y = y + [float(1.0 / 8 * (i - CUTOFF_VOLTAGE))
-                 for i in x[flatnonzero(x == 0.0)[0]:]]
-        # y.append(maxint)
-        print len(y), len(x)
+        y += sorted(
+            [
+                (1000.0 / 8 * abs(i - CUTOFF_VOLTAGE))
+                for i in x[flatnonzero(x == 0.0)[0]:]
+            ]
+        )
+
+        ax.set_xlim([0, 2])
+        ax.set_ylim([-1, 150])
 
     else:
         x = arange(-10.0, 0, 0.01)
         y = [0] * len(x)
         y[-1] = maxint
+        ax.set_ylim([-1, 5])
+        ax.set_xlim([-10, 10])
 
-    plt.plot(x, sorted(y), '-r', label='I-V curve')
-    ax.set_xlim([-10, 10])
-    ax.legend(loc='upper center', shadow=True)
+    plt.plot(x, y, '-r', label='I-V curve')
+    ax.legend(loc='upper right', shadow=True)
     fig.savefig(fig_path + fig_name)
 
 
 if __name__ == '__main__':
 
-    # plot_data_IV('Vd_1', 'Id_1', 'I-V Curve of Figure 1',
-    #              'figure3.png', 'Turn-on voltage')
-    # plot_data_IV('Vd_2', 'Id_2', 'I-V Curve of Figure 2',
-    #              'figure4.png', 'Breakdown voltage')
+    plot_data_IV('Vd_1', 'Id_1', 'I-V Curve of Figure 1',
+                 'figure3.png', 'Turn-on voltage')
+    plot_data_IV('Vd_2', 'Id_2', 'I-V Curve of Figure 2',
+                 'figure4.png', 'Breakdown voltage')
 
-    # plot_ideal_IV('Ideal Diode Model', 'test_fig.png')
-    plot_ideal_IV('Ideal Diode Model', 'test_fig1.png', True)
+    plot_ideal_IV('Ideal Diode Model', 'ideal.png')
+    plot_ideal_IV('Piecewise Linear Diode Model', 'piecewise.png', True)
