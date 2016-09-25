0b15d550ae938e8ccf3c20d0457bedc931ffc1e8
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-09-25 00:38:15 -0400

    add convolution sum plots

--- labs/lab03/scripts/pulse_conv.m
+++ labs/lab03/scripts/pulse_conv.m
@@ -8,8 +8,8 @@ function pulse_conv(t, x, h, sec)
         end;
     end;
 
-    plot_conv(t, x, '$$x(t)$$', ['3.', sec, '.1']);
-    plot_conv(t, h, '$$h(t)$$', ['3.', sec, '.2']);
+    plot_conv(t, x, '$$x(t)$$', ['3.', sec, '_1']);
+    plot_conv(t, h, '$$h(t)$$', ['3.', sec, '_2']);
     plot_conv(t, y_hat, '$$\hat{y} = \sum_{k=-\infty}^{\infty}{x_{n-k}h_{k}}$$', ['3.', sec, '_3']);
 
 end
