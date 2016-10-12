64342a0a08267b12d3b3b480f6748424824eae1d
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-12 14:55:07 -0400

    add lecture 11, work on lab4 report

--- labs/lab04/scripts/cexp_response.m
+++ labs/lab04/scripts/cexp_response.m
@@ -1,18 +1,31 @@
 function response = cexp_response(h,t,f,ifploton)
-% function response = cexp_response(h,t,f,ifploton)
-%  This function plots the response of an LTI system described by impulse
-% response h sampled at the times in t, at the frequencies given in f.
-%
-% Calling parameters
-%  (1 x N) array of impulse response.
-%  (1 x N) array of sample times in seconds
-%  (1 x M) array of evaluation frequencies in Hz
-%  Note M is not necessarily equal to N
-%  ifploton = 1 for plots, 0 otherwise
-%    ifploton = 1 will plot the magnitude and phase of the response at the
-%    response frequencies (in Hz)
-%
-% Returned parameters
-%  response (1 x M) array of complex outputs representing the steady state output at each 
-%   of the M frequencies.
+    % function response = cexp_response(h,t,f,ifploton)
+    %  This function plots the response of an LTI system described by impulse
+    % response h sampled at the times in t, at the frequencies given in f.
+    %
+    % Calling parameters
+    %  (1 x N) array of impulse response.
+    %  (1 x N) array of sample times in seconds
+    %  (1 x M) array of evaluation frequencies in Hz
+    %  Note M is not necessarily equal to N
+    %  ifploton = 1 for plots, 0 otherwise
+    %    ifploton = 1 will plot the magnitude and phase of the response at the
+    %    response frequencies (in Hz)
+    %
+    % Returned parameters
+    %  response (1 x M) array of complex outputs representing the steady state output at each 
+    %   of the M frequencies.
 
+    t2 = -2 : dt : 20;
+
+    u = @(t) (t >= 0);
+    pulse = @(t, T) (u(t) - u(t - T));
+    x = @(t, f) (exp(2j*pi*f*t).*u(t));
+
+    figure;
+    for index = 1 : numel(f)
+        plot(t2, dt*abs(conv(x(t, f(index)), h)));
+        title(['Frequency: ' num2str(freq(index))])
+    end
+
+end
--- labs/lab04/scripts/simple_filter.m
+++ labs/lab04/scripts/simple_filter.m
@@ -1,24 +1,29 @@
-t = -1 : 1/1000: 10;
+dt = 1/1000;
+t = -1 : dt : 10;
+t2 = -2 : dt : 20;
 freq = [0 1 4 6.5 7.5 8.5 11.5 19.5];
 
 u = @(t) (t >= 0);
 pulse = @(t, T) (u(t) - u(t - T));
 x = @(t, f) (exp(2j*pi*f*t).*u(t));
 h = pulse(t, 0.25);
+disp(length(h))
+% h = h(h ~= 0);
+disp(length(h))
 
 figure;
 for index = 1 : numel(freq) / 2
-    subplot_mat = ['41' num2str(index)];
-    subplot(subplot_mat);
-    plot(abs(conv(x(t, freq(index)), h)));
+    subplot(['41' num2str(index)]);
+    plot(t2, dt*abs(conv(x(t, freq(index)), h)));
     title(['Frequency: ' num2str(freq(index))])
+    xlim([0 10]);
 end
 
 figure;
 for index = numel(freq) / 2 + 1 : numel(freq)
-    subplot_mat = ['41' num2str(index - 4)];
-    subplot(subplot_mat);
-    plot(abs(conv(x(t, freq(index)), h)));
+    subplot(['41' num2str(index - 4)]);
+    plot(t2, dt*abs(conv(x(t, freq(index)), h)));
     title(['Frequency: ' num2str(freq(index))])
+    xlim([0 10]);
 end
 
