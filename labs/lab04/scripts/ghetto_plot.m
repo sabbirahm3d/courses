1331dc3eb87521880f96892b93421f7593c9167c
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-13 14:17:52 -0400

    lab4 scripts half done

--- labs/lab04/scripts/ghetto_plot.m
+++ labs/lab04/scripts/ghetto_plot.m
@@ -2,8 +2,13 @@ function ghetto_plot(x, dt, t, t2, freq, h, xlimit)
 
     figure;
     for index = 1 : numel(freq) / 2
+
         subplot(['41' num2str(index)]);
         y = dt*abs(conv(x(t, freq(index)), h));
+
+        transient = t(h ~= 0);
+        disp(transient(end))
+
         plot(t2, y);
         title(['Frequency: ' num2str(freq(index))])
         if xlimit
@@ -15,6 +20,9 @@ function ghetto_plot(x, dt, t, t2, freq, h, xlimit)
     for index = numel(freq) / 2 + 1 : numel(freq)
         subplot(['41' num2str(index - 4)]);
         y = dt*abs(conv(x(t, freq(index)), h));
+        transient = t(h ~= 0);
+        disp(transient(end))
+
         plot(t2, y);
         title(['Frequency: ' num2str(freq(index))])
         if xlimit
