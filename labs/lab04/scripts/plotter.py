06768bd169241864b36b78f802c3a3beedf2c71a
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-19 00:04:14 -0400

    added simulated plot

--- labs/lab04/scripts/plotter.py
+++ labs/lab04/scripts/plotter.py
@@ -2,6 +2,7 @@ from os.path import abspath, dirname
 from os.path import join as path_join
 
 import matplotlib.pyplot as plt
+from numpy import arange, linspace
 from pandas import read_csv
 import seaborn as sns
 
@@ -16,19 +17,23 @@ def import_dataset(dataset):
     return df
 
 
-def plot_waves(v_bb, v_cc, v_ce, i_c, title, fig_name):
+def plot_waves(df, title, fig_name):
 
     sns.set_style(style='darkgrid')
     fig, ax = plt.subplots(nrows=1, ncols=1)
 
+    color = iter(plt.cm.rainbow(linspace(0, 1, 7)))
+
+    for i in arange(0, 3.5, 0.5):
+        new_df = df[df['v_bb'] == i]
+        i_b = str(round(i / 220000, 8))
+        plt.plot(new_df['v_ce'], new_df['i_c'],
+                 c=next(color),
+                 label=r'$i_b = {}\ A$'.format(i_b))
+
     plt.title(title, fontsize=20)
     plt.ylabel(r'$Collector\ Current,\ i_{C}\ (mA)$')
     plt.xlabel(r'$Collector-Emitter\ Voltage,\ V_{CE}\ (V)$')
-
-    plt.plot(v_bb, v_cc, '-b', label='Input Voltage')
-    plt.plot(v_ce, i_c, '-r', label=' Voltage')
-    plt.plot(v_ce, i_c, 'og', label=' Voltage')
-
     plt.xlim(0, 10)
     lgd = ax.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
 
@@ -46,12 +51,10 @@ if __name__ == '__main__':
         df.drop('neg i_c', axis=1, inplace=True)
         df.to_csv(path_join(BASE_DIR, 'data', 'c.csv'), index=False)
 
-    # v_bb, v_cc, v_ce, i_c = list(df)
-    # new_df = df[df['v_cc'] == 5]
-    # print new_df.head()
+    plot_waves(df, 'Simulated BJT Characteristic Curve', 'test')
 
-    plot_waves(new_df[v_ce], new_df[i_c], df[v_ce], df[i_c],
-               'Simulated BJT Characteristic Curve', 'test')
+    # new_df = df[df['v_cc'] == 10]
+    # print new_df.head()
 
     # df = import_dataset('a.csv')
     # df['i_c'] = abs(df['v_cc'] - df['v_ce']) / 2200
