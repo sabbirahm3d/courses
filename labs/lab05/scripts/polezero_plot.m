37a96b03ecb9e449c2464b2d5141f1e375c8155b
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-20 20:28:20 -0400

    polished pole zero plot

--- labs/lab05/scripts/polezero_plot.m
+++ labs/lab05/scripts/polezero_plot.m
@@ -20,36 +20,75 @@ function [  ] = polezero_plot( b,a,s_or_z,ROC)
     % if s_or_z = 'z', that is, the z­plane implementation will be provided at
     % a later time.
 
+    % Throw an error if option for plotting on the z-plane is selected
     if s_or_z == 'z'
         error('Z-plane not implemented yet');
 
     elseif s_or_z == 's'
 
+        % zeros and poles from the coefficients
         zeros_ = roots(b);
         poles_ = roots(a);
-        data_points = cat(1, zeros_, poles_);
 
+        % sort all the roots by their imaginary parts to determine the
+        % bounds on the imaginary axis
+        data_points = cat(1, zeros_, poles_);
         [~, idx] = sort(imag(data_points));
         sorted_imag = imag(data_points(idx));
 
-        [im_min, im_max] = deal(sorted_imag(1) - 1, sorted_imag(end) + 1);
+        % lower and upper bound of the imaginary axis
+        [im_min, im_max] = deal(ceil(sorted_imag(1)) - 1, ceil(sorted_imag(end)) + 1);
 
+        [re_min, re_max] = deal(ROC(1), ROC(end));
+
+        % special conditions for infinite bounds of the region of
+        % convergence
+        zoom = 0;  % determines if the real axis needs to be manipulated
+        if any(ROC == -inf) || any(ROC == inf)
+            [~, idx] = sort(real(data_points));
+            sorted_real= real(data_points(idx));
+            zoom = 1;
+            if any(ROC == -inf)
+                [re_min, re_max] = deal(ceil(sorted_real(1)) - 1, ROC(end));                
+            elseif any(ROC == inf)
+                [re_min, re_max] = deal(ROC(1), ceil(sorted_real(end)) + 1);
+            end
+        end
+        
+        figure;
         hold on;
 
-        positive_area = area(ROC, [im_max, im_max]);
-        negative_area = area(ROC, [im_min, im_min]);
+        % shading the region of convergence
+        positive_area = area([re_min, re_max], [im_max, im_max]);
+        negative_area = area([re_min, re_max], [im_min, im_min]);
+        % additional settings for the region of convergence
         set(positive_area, 'FaceColor', 'g');
         set(negative_area, 'FaceColor', 'g');
         alpha(0.5);
 
+        % plot the zeros and plots on the same plot
         plot(zeros_, 'or');
         plot(poles_, 'xb');
+
+        % if zooming in is required to fit the region of convergence
+        if zoom
+            xlim([re_min, re_max]);
+        end
+
+        % convert the coefficients into a symbolic polynomial, casted into
+        % LaTex formatted string to be used for the title of the plot
+        syms s;
+        poly_equ = latex(poly2sym(b, s) / poly2sym(a, s));
+        title_str = ['$$H(s)=', poly_equ, '$$'];
+
+        % labels for the plot
         ylabel('$$\j\omega=Im(s)$$', 'Interpreter', 'latex', 'FontSize', 18);
         xlabel('$$\sigma=Re(s)$$', 'Interpreter', 'latex', 'FontSize', 18);
-        title('Pole-Zero Plot', 'FontSize', 16);
+        title(title_str, 'Interpreter', 'latex', 'FontSize', 16);
 
         hold off;
 
+    % throw an error if an invalid option for plane is selected
     else
         error('Select a valid plane to plot');
 
