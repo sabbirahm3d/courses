fa425dc14085c62a70a840f8c5998199605bc561
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-26 13:10:05 -0400

    hardcoded load lines lol

--- labs/lab05/scripts/plotter.py
+++ labs/lab05/scripts/plotter.py
@@ -18,15 +18,16 @@ def plot_waves(title, fig_name):
     r_e = 424.24
     v_ce = arange(0, 8, 0.01)
     i_c_dc = [(8 - i) / (r_c + r_e) for i in v_ce]
-    i_c_dc = [(8 - i) / (r_c + r_e) for i in v_ce]
+    i_c_ac = [0.0035 + (-i / (r_c)) for i in v_ce]
 
-    plt.plot(v_ce, i_c_dc, label=r'$Load\ line$')
+    plt.plot(v_ce, i_c_dc, '-r', label='DC Load line')
+    plt.plot(v_ce, i_c_ac, '-b', label='AC Load line')
 
     plt.title(title, fontsize=20)
     plt.ylabel(r'$Collector\ Current,\ i_{C}\ (A)$')
     plt.xlabel(r'$Collector-Emitter\ Voltage,\ V_{CE}\ (V)$')
 
-    # plt.xlim(0, 10)
+    plt.ylim([0, 0.004])
     lgd = ax.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
 
     fig.savefig(fig_path + fig_name + '.png',
@@ -36,4 +37,4 @@ def plot_waves(title, fig_name):
 
 if __name__ == '__main__':
 
-    plot_waves('test', 'test')
+    plot_waves('Load Lines of the Common Emitter Circuit', 'fig5')
