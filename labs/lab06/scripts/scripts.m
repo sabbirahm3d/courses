6d2922a4746960d4d0b185daee61d43f7244cec7
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-11-06 18:58:04 -0500

    add 15 figures

--- labs/lab06/scripts/scripts.m
+++ labs/lab06/scripts/scripts.m
@@ -1,17 +1,17 @@
 % computing the coefficients
 x = zeros(length(t), 1);
-for kn = 1:11
-    k = kn * T - 8;
+for kn = 1:16
+    k = kn * 4 - 16;
     x(:,kn) = p(t + 0.5 - k);
 end
 
 x = sum(x, 2)';
 
 figure;
-plot(t, x')
+plot(t, x)
 xlabel('Time');
 ylabel('Amplitude');
-title('$$x(t) = \sum_{n=-N}^{N}{p(t-nT)}$$', 'Interpreter', 'latex', 'FontSize', 15);
+title('$$x(t) = \sum_{n=-3}^{3}{p(t-nT)}$$', 'Interpreter', 'latex', 'FontSize', 15);
 
 k = -800:800;
 ck = (1/T).* sinc(k/T);
