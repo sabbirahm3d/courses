dabd2f9c14a4131d0ede403cd8c8363b89414e3e
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-12-12 13:36:34 -0500

    add final gain waveform

--- project/scripts/plotter.py
+++ project/scripts/plotter.py
@@ -6,25 +6,47 @@ from pandas import read_csv
 import seaborn as sns
 
 BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
-fig_path = path.join(BASE_DIR, 'figures/')
+fig_path = path.join(BASE_DIR, 'statics/')
 
 df = read_csv("final_output_pspice.csv")
 
 sns.set_style(style='whitegrid')
 fig, ax = plt.subplots(nrows=1, ncols=1)
-
-plt.plot(1000 * df["t"], 2763 * df["v"], 'r-',
-         label=r"$Output\ voltage,\ v_{o}$")
 Fs = 8000
 f = 3.6
 sample = len(df["t"])
 x = arange(sample)
 y = sin(2 * pi * f * x / Fs)
-plt.plot(1000 * df["t"], 1000 * 0.001 * y, 'b-',
-         label=r"$Input\ voltage,\ v_{i}$")
 
-plt.title("Final Circuit Gain", fontsize=20)
-plt.ylabel(r'$Voltage\ (mV)$')
-plt.xlabel(r'$Time\ (ms)$')
-lgd = ax.legend(loc=1)
-fig.savefig('final_gain.png', bbox_extra_artists=(lgd,))
+
+def final_gain():
+
+    plt.plot(1000 * df["t"], 2763 * df["v"], 'r-',
+             label=r"$Output\ voltage,\ v_{o}$")
+    plt.plot(1000 * df["t"], 1000 * 0.001 * y, 'b-',
+             label=r"$Input\ voltage,\ v_{i}$")
+
+    plt.title("Final Circuit Gain", fontsize=20)
+    plt.ylabel(r'$Voltage\ (mV)$')
+    plt.xlabel(r'$Time\ (ms)$')
+
+
+def audio_max():
+
+    # plt.plot(1000 * df["t"], 5000 * df["v"], 'r-',
+    #          label=r"$Output\ voltage,\ v_{o}$")
+    plt.plot(1000 * df["t"], 0.001 * y, 'b-',
+             label=r"$Input\ voltage,\ v_{i}$")
+    plt.plot(1000 * df["t"], 2.4 * y, 'r-',
+             label=r"$Input\ voltage,\ v_{i}$")
+
+    plt.title("Final Circuit Gain", fontsize=20)
+    plt.ylabel(r'$Voltage\ (mV)$')
+    plt.xlabel(r'$Time\ (ms)$')
+
+
+# final_gain()
+audio_max()
+lgd = ax.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
+fig.savefig(path.join(fig_path, 'audio_max.png'),
+            bbox_extra_artists=(lgd,), bbox_inches='tight')
