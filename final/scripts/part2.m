2651b7e76122938d79dde7b733a6d059da9ef887
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-12-18 14:07:55 -0500

    fuck yeah progree

--- final/scripts/part2.m
+++ final/scripts/part2.m
@@ -1,26 +1,38 @@
 % import global variables    
+clear;
 settings_2;
 
-% plot_fft(d(t3), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
-% plot_fft(c(t3).*d(t), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
+nyquist_rate = 10*2*f2;
+fsa = 100000;
+dt = 1/fsa;
+t3 = [-5000:4999]*dt; % an array with 2^M points, but starting at M=-4096
+NFFT = length(t3)
+trecord = NFFT*dt; % total measurement time
+fresolution = 1/trecord % the resolution is 1/(measurement time)
+df = fresolution; %...which is also the frequency resolution
+
+funshifted= [0:NFFT-1]*df; % FFTs go from 0 to (N-1)df
+f = [-NFFT/2: NFFT/2-1]*df;  % centered version
 
 figure;
-plot(t0, d(t0), 'r-')
-title(...
-    '$$d(t)=A(1 + a_{1}cos(2\pi f_{1}t+\phi_{1}) + a_{2}cos(2\pi f_{2}t+\phi_{2}))$$',...
-    'interpreter', 'latex', 'fontsize', 16);
-ylabel('Amplitude');
-xlabel('Time (s)');
-
+plot(t3, d(t3));
+xlim([-0.1 0.1])
 figure;
 
 fft_d = fft(c(t3).*d(t3))*dt;
 fft_d = fftshift(fft_d);
 
 subplot(211);
-plot(f, abs(fft_d));
-xlim([-5 5])
+plot(f, abs(fft_d), 'r-', 'linewidth', 2);
+xlim([-200 200]);
+title(['Frequency resolution: ', num2str(fresolution), ' Hz'],...
+    'fontsize', 16)
+ylabel('Magnitude of D(f)');
 
 subplot(212);
-plot(f, angle(fft_d));
-xlim([-5 5])
+ph_adj = exp(2j*pi*min(t3)*f).*fft_d; % adjust for first sample not at t=0
+plot(f, angle(ph_adj)/pi, 'b-', 'linewidth', 2);
+xlim([-200 200]);
+ylabel('Phase of D(f)');
+
+xlabel('Frequency (Hz)');
--- final/scripts/settings_2.m
+++ final/scripts/settings_2.m
@@ -8,24 +8,7 @@ f2 = 150;
 phi1 = -pi/2;
 phi2 = -pi/2;
 
-dt = 1/(10*300);
-t0 = -0.05:dt:0.05;
-
-fsa=1000;
-dt=1/fsa;
-t3=[-4096:4095]*dt; % an array with 2^M points, but starting at M=-4096
-NFFT = length(t3);
-trecord = NFFT*dt; % total measurement time
-fresolution = 1/trecord; % the resolution is 1/(measurement time)
-df = fresolution; %...which is also the frequency resolution
-
-funshifted=[0:NFFT-1]*df; % FFTs go from 0 to (N-1)df
-f = [-NFFT/2: NFFT/2-1]*df;  % centered version
-
 f_c = 100000;
 
 d = @(t) (A*(1 + a1*cos(2*pi*f1*t+phi1) + a2*cos(2*pi*f2*t+phi2) ));
 c = @(t) (cos(2*pi*f_c*t));
-
-theory_d = @(f) (f*0.0001);
-
