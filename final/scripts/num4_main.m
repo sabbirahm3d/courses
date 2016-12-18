4327a370bccc4d6c58c9c721f6ea87a608bf3cbd
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-12-18 12:35:38 -0500

    now is the hard part

--- final/scripts/num4_main.m
+++ final/scripts/num4_main.m
@@ -17,7 +17,7 @@ x_tick_str{11} = '\tau_3 + (\tau_1 + \tau_2)/2';
 figure;
 
 subplot(311);
-plot(t2, dt*conv(a(t + tau3/2 - beta, tau1), heaviside(t)));
+plot(t2, dt*conv(a(t + tau3/2 - beta, tau1), heaviside(t)), 'LineWidth', 2);
 
 xlim([-0.5 tau2+tau3]);
 ylim([0 1.5]);
@@ -26,7 +26,7 @@ ylabel('$$y_{1}(t-\beta)$$', 'Interpreter', 'latex', 'FontSize', 16);
 set(gca, 'xTickLabel', x_tick_str, 'yTickLabel', y_tick_str);
 
 subplot(312);
-plot(t2, dt*conv(a(t - tau3/2 - beta, tau2), heaviside(t)));
+plot(t2, dt*conv(a(t - tau3/2 - beta, tau2), heaviside(t)), 'LineWidth', 2);
 
 xlim([-0.5 tau2+tau3]);
 ylim([0 1.5]);
@@ -35,7 +35,7 @@ ylabel('$$y_{2}(t-\beta)$$', 'Interpreter', 'latex', 'FontSize', 16);
 set(gca, 'xTickLabel', x_tick_str, 'yTickLabel', y_tick_str);
 
 subplot(313);
-plot(t2, w(t, tau1, tau2, tau3));
+plot(t2, w(t, tau1, tau2, tau3), 'LineWidth', 2);
 
 xlim([-0.5 tau2+tau3]);
 ylim([0 1.5]);
@@ -44,26 +44,3 @@ ylabel('$$y_{1}(t-\beta) - y_{2}(t-\beta)$$', ...
 xlabel('Time (s)')
 
 set(gca, 'xTickLabel', x_tick_str, 'yTickLabel', y_tick_str);
-
-% figure;
-% subplot(211);
-% fft_w1 = fft(a(t + tau3/2, tau1));
-% fft_w1 = fftshift(fft_w1);
-% 
-% fft_w2 = fft(heaviside(t));
-% fft_w2 = fftshift(fft_w2);
-% 
-% fft_w3 = fft(a(t - tau3/2, tau2));
-% fft_w3 = fftshift(fft_w3);
-% 
-% plot(t, abs(fft_w1.*fft_w2 - fft_w3.*fft_w2));
-% xlim([-0.1 0.1])
-% 
-% fft_w0 = fft(w(t, tau1, tau2, tau3));
-% fft_w0 = fftshift(fft_w0);
-% 
-% subplot(212)
-% plot(t2, abs(fft_w0));
-% xlim([-0.1 0.1])
-% 
-
--- final/scripts/part2.m
+++ final/scripts/part2.m
@@ -1,5 +1,26 @@
 % import global variables    
 settings_2;
 
-plot_fft(d(t), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
-plot_fft(c(t).*d(t), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
+% plot_fft(d(t3), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
+% plot_fft(c(t3).*d(t), theory_d(f), 'p_{1}', '$$\tau sinc(f\tau)$$');
+
+figure;
+plot(t0, d(t0), 'r-')
+title(...
+    '$$d(t)=A(1 + a_{1}cos(2\pi f_{1}t+\phi_{1}) + a_{2}cos(2\pi f_{2}t+\phi_{2}))$$',...
+    'interpreter', 'latex', 'fontsize', 16);
+ylabel('Amplitude');
+xlabel('Time (s)');
+
+figure;
+
+fft_d = fft(c(t3).*d(t3))*dt;
+fft_d = fftshift(fft_d);
+
+subplot(211);
+plot(f, abs(fft_d));
+xlim([-5 5])
+
+subplot(212);
+plot(f, angle(fft_d));
+xlim([-5 5])
