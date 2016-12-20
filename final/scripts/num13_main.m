d01a2aed4f2a39bfe964ea58e298c3d7e1a0be34
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-12-19 22:06:45 -0500

    fix script names

--- final/scripts/num13_main.m
+++ final/scripts/num13_main.m
@@ -1,21 +1,48 @@
-a_p = 3;
-a_s = 64;
-f_p = 10e3;
-f_s = 50e3;
-n = 5;
+figure;
 
-nu_s = f_s/f_p;
-epsilon = (10^(a_p/10)-1)^(1/2);
-nu_3 = (1/epsilon)^(1/n);
+% plot a unit circle to envelope the poles
+th = 0:pi/50:2*pi;
+xunit = cos(th);
+yunit = sin(th);
+h1 = plot(xunit, yunit);
+hold on;
+h2 = plot(s, 'rx', 'linewidth', 2);
 
-theta = zeros(1, n);
-s = zeros(1, n);
+xlim([-1.5 1.5]);
+ylim([-1.5 1.5]);
+title('Pole-Zero Plot of H(s)')
+ylabel('$$\frac{im(s)}{\omega_c}$$', 'interpreter', 'latex');
+xlabel('$$\frac{re(s)}{\omega_c}$$', 'interpreter', 'latex');
+legend(h2, {'Poles'});
 
-for k = 1:n
-    theta(k) = (2*(k-1)+n-1)*pi/(2*n);
-    s(k) = nu_3*exp(-1j*theta(k));
-end
+b = [1 (2.6193+1.903i) (1.602+4.985i) -(1.620-4.987i) -(2.623-1.906) - 1.002];
+a = [0 0 0 0 0 w_cn];
 
-w_c = prod(s, 2)^(1/n);
-w_cn = w_c^n;
-s
+% http://stackoverflow.com/questions/28978671/matlab-quickie-how-to-plot-markers-on-a-freqs-plot
+
+w = logspace(-3,5); % Frequency vector
+
+%// New code
+h = freqs(b,a,w); %// Output of freqs
+mag = 20*log10(abs(h)); %// Magnitude in dB
+pha = (180/pi)*angle(h); %// Phase in degrees
+
+% %// Declare points
+% wpt = [600, 7500];
+% mpt = [20, -71];
+
+%// Plot the magnitude as well as markers
+figure;
+subplot(2,1,1);
+semilogx(w, -mag, 'linewidth', 2);
+ylim([-20 0])
+xlabel('Frequency');
+ylabel('Magnitude (dB)');
+grid;
+
+%// Plot phase
+subplot(2,1,2);
+semilogx(w, pha, 'color', [0 0.6 0], 'linewidth', 2);
+xlabel('Frequency');
+ylabel('Phase (Degrees)');
+grid;
