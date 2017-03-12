9fca847b33bb5edef696426f7d4d1eaa9cf499f4
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-03-12 14:47:02 -0400

    update report

--- project/proj1/main.R
+++ project/proj1/main.R
@@ -3,35 +3,36 @@ library(ggplot2)
 x <- 20
 N <- 1000
 p <- 0.4
-generated_data <- rbinom(N, x, p)
+generatedData <- rbinom(N, x, p)
 
 # a
 png(filename="hist.png")
-ggplot() + aes(generated_data) + 
+ggplot() + aes(generatedData) + 
   geom_histogram(binwidth=1, colour="black", fill="white")
 dev.off()
 
-# # b
-# paste("Mean:", signif(mean(generated_data), digits=4),
-#     "| Variance:", signif(var(generated_data), digits=4),
-#     "| Standard Deviation:", signif(sqrt(var(generated_data)), digits=4)
-# )
+# b
+# print out the mean, variance and standard deviation
+paste("Mean:", signif(mean(generatedData), digits=4),
+    "| Variance:", signif(var(generatedData), digits=4),
+    "| Standard Deviation:", signif(sqrt(var(generatedData)), digits=4)
+)
 
-# # c
-# summary(generated_data)
-# relfreq <- as.vector(ftable(generated_data))/N
-# # ftable(generated_data)
+# c
+# print out the summary statistics
+summary(generatedData)
+relfreq <- as.vector(ftable(generatedData))/N
 
-# ftable(generated_data)/N
+ftable(generatedData)/N
 
-# # d
-# png(filename="relfreq.png")
-# barplot(relfreq, ylab="y")
-# # ggplot() + aes(x=c(1:length(relfreq))) + 
-# #   geom_histogram(y=relfreq, binwidth=1, colour="black", fill="white")
-# dev.off()
+# d
+png(filename="relfreq.png")
+barplot(relfreq, ylab="y")
+# ggplot() + aes(x=c(1:length(relfreq))) + 
+#   geom_histogram(y=relfreq, binwidth=1, colour="black", fill="white")
+dev.off()
 
-# # e
+# e
 
 
 # 3.58
