264afad88b9a4e40cca691868dcfb7c788959460
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-04-09 17:41:55 -0400

    major update to proj2

--- project/proj2/main.R
+++ project/proj2/main.R
@@ -4,31 +4,58 @@
 # WARNINGS OR ERRORS IN OTHER SYSTEMS
 
 library(ggplot2)  # for generating high quality plots
+set.seed(124)
+
+outputTemplate <- "\\subsection{Output}
+
+    \\begin{table}[h]
+        \\centering
+        \\begin{tabular*}{200pt}{@{\\extracolsep{\\fill}} c c}
+
+        \\textbf{Actual}: & \\textbf{Theoretical}: \\\\
+        $\\mu$: %1.0f  & $\\mu$: %1.0f \\\\
+        E($\\overline{X}$): %.4f & E($\\overline{X}$): %.4f \\\\
+        $\\sigma$: %1.0f & $\\sigma$: %1.0f \\\\
+        $\\sigma$\\textsubscript{$\\overline{X}$}: %.3f & $\\sigma$\\textsubscript{$\\overline{X}$}: %.3f \\\\
+
+        \\end{tabular*}
+    \\end{table}
+"
 
 # ------------------------------ Part 1 ------------------------------
 
 # initialize parameters for normal distribution
-N <- 40
-mu <- 3
-sigma <- 2
-# set.seed(124)
-samp_means <- c()
+N <- 40  # size
+mu <- 3  # mean
+sigma <- 2  # standard deviation
+
+sampMeans <- c()  # initialize empty array
+# generate 1000 samples
 for (i in 1:1000){
     generatedData <- rnorm(N, mu, sigma)
-    samp_means[i] = mean(generatedData)
+    # store the sample means in vector
+    sampMeans[i] = mean(generatedData)
 }
 
-print(mean(samp_means))
-print(sd(samp_means))
-print(sigma/sqrt(N))
-# ------ a ------
 
-png(filename="figures/hist.png")
+# save output
+sink("part1.tex", append=FALSE, split=FALSE)
+cat(
+    sprintf(
+        outputTemplate,
+        mu, mu,
+        mean(sampMeans), mean(sampMeans),
+        sigma, sigma,
+        sd(sampMeans), sigma/sqrt(N)
+    )
+)
+sink()
 
+png(filename="figures/hist1.png")
 # plot a histogram of the data
-ggplot() + aes(samp_means) + 
-    geom_histogram(binwidth=1, color="black", fill="white") +
-    labs(y="Count")
+ggplot() + aes(sampMeans) + 
+    geom_histogram(binwidth=0.1, color="black", fill="white") +
+    labs(y="Count", x="Sample Means")
 
 dev.off()
 
@@ -36,29 +63,38 @@ dev.off()
 # ------------------------------ Part 2 ------------------------------
 
 # initialize parameters for binomial distribution
-x <- 15
-N <- 10
+N <- 15
+n <- 10
 p <- 0.15
-# set.seed(124)
-samp_means <- c()
+sampMeans <- c()
 for (i in 1:1000){
-    generatedData <- rbinom(N, x, p)
-    samp_means[i] = mean(generatedData)
+    generatedData <- rbinom(N, n, p)
+    sampMeans[i] = mean(generatedData)
 }
 
-print(mean(samp_means))
-print(x*p)
-print(sd(samp_means))
-print(sqrt(x*p*(1-p)))
-print(sigma/sqrt(N))
-# ------ a ------
 
-png(filename="figures/hist1.png")
+# save output
+sink("part2.out", append=FALSE, split=FALSE)
+
+# save output
+sink("part2.tex", append=FALSE, split=FALSE)
+cat(
+    sprintf(
+        outputTemplate,
+        n*p, n*p,
+        mean(sampMeans), mean(sampMeans),
+        n*p*(1-p), n*p*(1-p),
+        sd(sampMeans), sd(sampMeans)
+    )
+)
+sink()
+
+png(filename="figures/hist2.png")
 
 # plot a histogram of the data
-ggplot() + aes(samp_means) + 
-    geom_histogram(binwidth=1, color="black", fill="white") +
-    labs(y="Count")
+ggplot() + aes(sampMeans) + 
+    geom_histogram(binwidth=0.2, color="black", fill="white") +
+    labs(y="Count", x="Sample Means")
 
 dev.off()
 
@@ -66,28 +102,37 @@ dev.off()
 # ------------------------------ Part 3 ------------------------------
 
 # initialize parameters for binomial distribution
-x <- 120
-N <- 10
+N <- 120
+n <- 10
 p <- 0.15
-# set.seed(124)
-samp_means <- c()
+sampMeans <- c()
 for (i in 1:1000){
-    generatedData <- rbinom(N, x, p)
-    samp_means[i] = mean(generatedData)
+    generatedData <- rbinom(n, N, p)
+    sampMeans[i] = mean(generatedData)
 }
 
-print(mean(samp_means))
-print(x*p)
-print(sd(samp_means))
-print(sqrt(x*p*(1-p)))
-print(sigma/sqrt(N))
-# ------ a ------
 
-png(filename="figures/hist2.png")
+# save output
+sink("part3.out", append=FALSE, split=FALSE)
+
+# save output
+sink("part3.tex", append=FALSE, split=FALSE)
+cat(
+    sprintf(
+        outputTemplate,
+        n*p, n*p,
+        mean(sampMeans), mean(sampMeans),
+        n*p*(1-p), n*p*(1-p),
+        sd(sampMeans), sd(sampMeans)
+    )
+)
+sink()
+
+png(filename="figures/hist3.png")
 
 # plot a histogram of the data
-ggplot() + aes(samp_means) + 
-    geom_histogram(binwidth=1, color="black", fill="white") +
-    labs(y="Count")
+ggplot() + aes(sampMeans) + 
+    geom_histogram(binwidth=0.3, color="black", fill="white") +
+    labs(y="Count", x="Sample Means")
 
 dev.off()
