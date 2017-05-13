46dfda1a9a80563aafeffa77f98bb2a60930cbee
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-05-13 13:29:16 -0400

    almost done with part 3

--- project/proj3/main.R
+++ project/proj3/main.R
@@ -3,7 +3,8 @@
 # NOTE: THIS SCRIPT WAS COMPILED ON A LINUX MACHINE - SOME STATEMENTS MAY THROW
 # WARNINGS OR ERRORS IN OTHER SYSTEMS
 
-library(ggplot2)  # for generating high quality plots
+library(ggplot2)
+library(gridExtra)
 set.seed(0)  # seed the random generators
 
 scoreTemplate <- 
@@ -12,34 +13,30 @@ scoreTemplate <-
     =\\frac{%0.1f-%0.1f}{\\sfrac{%0.1f}{\\sqrt{%d}}}=%0.4f
     \\end{equation*}"
 
-resultTemplate <- "\\begin{equation*}
-    \\because %s=%0.4f %s %0.4f
-    \\end{equation*}"
 
-dumpComputation <- function(X, mu, sigma, n, alpha, distType, outputFile) {
+dumpComputation <- function(X, mu, sigma, n, alpha, 
+    distType, twoSided, outputFile) {
 
     score <- (X - mu)/(sigma/sqrt(n))
 
-    tableStr <- ""
     tableVal <- 0
-    ineq <- ""
+    pScore <- 0
 
     if (distType == "Z") {
 
-        tableStr <- paste0(distType, "_{", alpha/2, "}")
         tableVal <- qnorm(alpha/2)
+        pScore <- 2 * (1 - (pnorm(score)))
 
     } else if (distType == "t") {
 
-        tableStr <- paste0(distType, "_{", alpha/2, ",", n-1, "}")
-        tableVal <- qt(alpha/2, df=n-1)
+        if (twoSided) {
+            tableVal <- qt(alpha/2, df=n-1)            
+            pScore <- 2 * (1 - pt(score,df=n-1))
+        } else {
+            tableVal <- qt(alpha, df=n-1)
+            pScore <- pt(score, df=n-1)
+        }
 
-    }
-
-    if (tableVal < score) {
-        ineq <- "<"
-    } else if (tableVal < score) {
-        ineq <- ">"
     }
 
     # dump output to LaTex modules
@@ -52,15 +49,9 @@ dumpComputation <- function(X, mu, sigma, n, alpha, distType, outputFile) {
             distType, X, mu, sigma, n, 
             score, distType)
     )
-    sink(
-        paste0("latex_mods/", outputFile, "_result.tex"),
-        append=FALSE, split=FALSE
-    )
-    cat(
-        sprintf(resultTemplate,
-            tableStr, tableVal, ineq, score)
-    )
     sink()  # return stdout to console
+    print(paste("Score:", tableVal))
+    print(paste("P-value:", pScore))
 
 }
 
@@ -74,7 +65,8 @@ sigma <- 2.1
 n <- 35
 alpha <- 0.05
 
-dumpComputation(X, mu, sigma, n, alpha, "Z", "part1")
+dumpComputation(X=X, mu=mu, sigma=sigma, n=n, 
+    alpha=alpha, distType="Z", twoSided=TRUE, "part1")
 
 
 # ------------------------------ Part 2 ------------------------------
@@ -84,7 +76,8 @@ mu <- 75
 s <- 7.9
 n <- 12
 
-dumpComputation(X, mu, s, n, alpha, "t", "part2")
+dumpComputation(X=X, mu=mu, sigma=s, n=n, 
+    alpha=alpha, distType="t", twoSided=FALSE, "part2")
 
 
 # ------------------------------ Part 3 ------------------------------
@@ -92,158 +85,21 @@ dumpComputation(X, mu, s, n, alpha, "t", "part2")
 weights <- c(66, 63, 64, 62, 65)
 X <- mean(weights)
 s <- sd(weights)
+n <- length(weights)
 mu <- 60
 
-dumpComputation(X, mu, s, length(weights), alpha, "t", "part3")
+weightsSeq <- seq(1,length(weights))
 
+data <- data.frame(weightsSeq, weights)
 
-# # global variables
-# NUMSAMPS <- 1000  # number of random samples per distribution
+dumpComputation(X=X, mu=mu, sigma=s, n=n, 
+    alpha=alpha, distType="t", twoSided=TRUE, "part3")
 
-# randDist <- function(N, a, b, distType, outputFile) {
-#     # Generates a random normal or binomial distribution.
-#     # 
-#     # Args:
-#     #   N: size of sample
-#     #   a: First distribution parameter.
-#     #       a = mu for normal distribution
-#     #       a = n for binomial distribution
-#     #   b: Second distribution parameter.
-#     #       b = sigma for normal distribution
-#     #       b = p for binomial distribution
-#     #   distType: Type of distribution.
-#     #       Options: "normal", "binomial"
-#     #   outputFile: Name of LaTex output file
-
-#     # initialize variables to hold data for the first sample
-#     firstMean <- firstStd <- 0
-
-#     # initialize distribution variables
-#     mu <- sigma <- n <- p <- 0
-
-#     # initialize empty arrays
-#     sampMeans <- generatedData <- rep(0, times=NUMSAMPS)
-
-#     # rename parameter values to distribution parameters for convenience
-#     if (distType == "normal") {
-#         mu <- a
-#         sigma <- b
-#     } else if (distType == "binomial") {
-#         n <- a
-#         p <- b
-#     }
-
-#     # generate 1000 samples
-#     for (i in 1:NUMSAMPS) {
-
-#         # generate distribution based on type chosen
-#         if (distType == "normal") {
-#             generatedData <- rnorm(N, mu, sigma)
-#         } else if (distType == "binomial") {
-#             generatedData <- rbinom(N, n, p)
-#         }
-
-#         # store the sample means in vector
-#         sampMeans[i] = sum(generatedData)/N
-
-#         if (i == 1) {
-
-#             # store the first sample mean
-#             firstMean = sum(generatedData)/N
-
-#             # store the first sample standard deviation
-#             if (distType == "normal") {
-#                 # sigma/sqrt(N) if normal
-#                 firstStd = sigma/sqrt(N)
-#             } else if (distType == "binomial") {
-#                 # sqrt(n*p*(1-p)/N) if binomial
-#                 firstStd = sqrt(n*p*(1-p)/N)
-#             }
-
-#         }
-
-#     }
-
-#     # generate templates based on the distribution type and computed values
-#     outputData <- ''
-#     if (distType == "normal") {
-#         outputData <- sprintf(
-#             scoreTemplate,
-#             firstMean, firstStd,
-#             "\\mu", "\\mu", "\\sigma", "\\frac{\\sigma}{\\sqrt{n}}",
-#             mu, mu,
-#             mean(sampMeans), mu,
-#             sigma, sigma,
-#             sd(sampMeans), sigma/sqrt(N)
-#         )
-#     } else if (distType == "binomial") {
-#         outputData <- sprintf(
-#             scoreTemplate,
-#             firstMean, firstStd,
-#             "np", "np", "\\sqrt{np(1-p)}", "\\sqrt{\\frac{np(1-p)}{N}}",
-#             n*p, n*p,
-#             mean(sampMeans), n*p,
-#             sqrt(n*p*(1-p)), sqrt(n*p*(1-p)),
-#             sd(sampMeans), sqrt(n*p*(1-p)/N)
-#         )
-#     }
-
-#     # dump output to LaTex modules
-#     sink(outputFile, append=FALSE, split=FALSE)
-#     cat(outputData)
-#     sink()  # return stdout to console
-
-#     return(sampMeans)
-
-# }
-
-
-# plotHist <- function(sampMeans, figureFile, binwidth) {
-#     # Plot a histogram of the data
-#     #
-#     # Args:
-#     #   sampMeans: the sample means generated from the random distributions
-#     #   figureFile: file name of the output plot
-#     #   binwidth: width of the bins of the histogram
-
-#     histPlot <- ggplot() + aes(sampMeans) + 
-#         geom_histogram(binwidth=binwidth, color="black", fill="white") +
-#         labs(y="Count", x="Sample Means")
-
-#     # save plot to filename
-#     ggsave(filename=paste0("figures/", figureFile), plot=histPlot)
-
-# }
-
-
-# # ------------------------------ Part 1 ------------------------------
-
-# # initialize parameters for normal distribution
-# N <- 40  # size
-# mu <- 3  # mean
-# sigma <- 2  # standard deviation
-
-# sampMeans <- randDist(N, mu, sigma, "normal", "part1.tex")
-# plotHist(sampMeans, "hist1.png", 0.1)
-
-
-# # ------------------------------ Part 2 ------------------------------
-
-# # initialize parameters for binomial distribution
-# N <- 15
-# n <- 10
-# p <- 0.15
-
-# sampMeans <- randDist(N, n, p, "binomial", "part2.tex")
-# plotHist(sampMeans, "hist2.png", 0.1)
-
-
-# # ------------------------------ Part 3 ------------------------------
-
-# # initialize parameters for binomial distribution
-# N <- 120
-# n <- 10
-# p <- 0.15
-
-# sampMeans <- randDist(N, n, p, "binomial", "part3.tex")
-# plotHist(sampMeans, "hist3.png", 0.025)
+probNormPlt <- ggplot(data, aes(sample=weights))+stat_qq()
+boxPlt <- ggplot(data, aes(x=weightsSeq, y=weights)) + geom_boxplot() + theme(axis.title.x=element_blank(),
+    axis.text.x=element_blank(),
+    axis.ticks.x=element_blank())
+# save plot to filename
+png(filename="figures/part3.png")
+grid.arrange(probNormPlt, boxPlt, ncol=2)
+dev.off()
