3291d8f1846bdd794d252e91c3fc0481b03fe40f
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-09-19 13:49:23 -0400

    start on code for UART

--- labs/lab03/uart.asm
+++ labs/lab03/uart.asm
@@ -1,77 +1,110 @@
-/*
- * uart.asm
- *
- */ 
+/* uart.asm */ 
 
-;Simple program to send "Hello! World" using AVR Butteryfly @ default 8Mhz
-;runs 4800 buad with 2 stop bits and no partiy
-.def TEMP=r16
-.def TEMP0=r17
-.def TEMP1=r18
+; Simple program to send "Hello! World" using AVR Butteryfly @ default 8Mhz
+; runs 4800 buad with 2 stop bits and no parity
+.DEF TEMP                       = R16
+.DEF TEMP0                      = R17
+.DEF TEMP1                      = R18
+
 ; Initialize Stack Pointer to end of RAM
-LDI TEMP, HIGH(RAMEND) ; Upper byte
-OUT SPH,TEMP ; to stack pointer
-LDI TEMP, LOW(RAMEND) ; Lower byte
-OUT SPL,TEMP; to stack pointer
-USART_Init:
+LDI TEMP, HIGH(RAMEND)          ; Upper byte
+OUT SPH, TEMP                   ; to stack pointer
+LDI TEMP, LOW(RAMEND)           ; Lower byte
+OUT SPL, TEMP                   ; to stack pointer
 
-;Load UBRRH with 0 and UBRRL with 103
-;in other words FOSC/16/BAUD-1
-;to set a baud rate of about 4800 at 8MHz
-ldi TEMP,00
-//mov UBRRH, TEMP ; can't do this since UBRRH is in the _extended_ i/o
-sts UBRRH,TEMP
-ldi TEMP,103
-sts UBRRL,TEMP
-;Clear all error flags
-ldi TEMP,00
-sts UCSRA,TEMP
-;Enable Transmission and Reception
-ldi TEMP,(1<<RXEN0)|(1<<TXEN0)
-sts UCSRB,TEMP
-; Set frame format: 8data, 2stop bit
-ldi TEMP, (1<<USBS0)|(3<<UCSZ00)
-sts UCSR0C,TEMP
+UART_INIT:
 
-;Transmit Data
-USART_Transmit_H:
-;Wait for empty transmit buffer
-;sbis UCSR0A,UDRE ; can't do this
-;rjmp USART_Transmit_H ; and this since UCSR0A is extended i/o
-lds TEMP0, UCSR0A
-sbrs TEMP0,UDRE
-rjmp USART_Transmit_H
-; send the data
-ldi TEMP, 0x48
-sts UDR0, TEMP
-USART_Transmit_I:
-; Wait for empty transmit buffer
-lds TEMP0, UCSR0A
-sbrs TEMP0,UDRE
-rjmp USART_Transmit_I
-; send the data
-ldi TEMP, 0x49
-sts UDR0, TEMP
-USART_Transmit_exclamation:
-; Wait for empty transmit buffer
-lds TEMP0, UCSR0A
-sbrs TEMP0,UDRE
-rjmp USART_Transmit_exclamation
-; send the data
-ldi TEMP, 0x21
-sts UDR0, TEMP
+    ; Load UBRRH with 0 and UBRRL with 103
+    ; in other words FOSC/16/BAUD-1
+    ; to set a baud rate of about 4800 at 8MHz
+    LDI TEMP, 00
+
+    ; MOV UBRRH, TEMP ; can't do this since UBRRH is in the _extended_ i/o
+    STS UBRRH, TEMP
+    LDI TEMP, 103
+    STS UBRRL, TEMP
+
+    ; Clear all error flags
+    LDI TEMP, 00
+    STS UCSRA, TEMP
+
+    ; Enable Transmission and Reception
+    LDI TEMP, (1 << RXEN0) | (1 << TXEN0)
+    STS UCSRB, TEMP
+
+    ; Set frame format: 8data, 2stop bit
+    LDI TEMP, (1 << USBS0) | (3 << UCSZ00)
+    STS UCSR0C, TEMP
+
+; Transmit Data
+UART_TRANSMIT_H:
+
+    ; Wait for empty transmit buffer
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT_H
+
+    ; send the data
+    LDI TEMP, 0x48              ; hex for 'H'
+    STS UDR0, TEMP
+
+UART_TRANSMIT_E:
+
+    ; Wait for empty transmit buffer
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT_E
+
+    ; send the data
+    LDI TEMP, 0x45              ; hex for 'E'
+    STS UDR0, TEMP
+
+UART_TRANSMIT_L:
+
+    ; Wait for empty transmit buffer
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT_L
+
+    ; send the data
+    LDI TEMP, 0x4C              ; hex for 'L'
+    STS UDR0, TEMP
+
+UART_TRANSMIT_O:
+
+    ; Wait for empty transmit buffer
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT_O
+
+    ; send the data
+    LDI TEMP, 0x4F              ; hex for 'O'
+    STS UDR0, TEMP
+
+UART_TRANSMIT_EXCLAMATION:
+
+    ; Wait for empty transmit buffer
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT_EXCLAMATION
+
+    ; send the data
+    LDI TEMP, 0x21              ; hex for '!'
+    STS UDR0, TEMP
 
 ; now send the data using a function call
-
 DONE:
-rjmp DONE
+    RJMP DONE
 
-USART_Transmit: ;assumes data is in register TEMP
-; Wait for empty transmit buffer
-;sbis UCSR0A,UDRE
-lds TEMP0, UCSR0A
-sbrs TEMP0,UDRE
-rjmp USART_Transmit
-; send the data
-sts UDR0, TEMP
-ret
+; assumes data is in register TEMP
+UART_TRANSMIT:
+
+    ; Wait for empty transmit buffer
+    ;sbis UCSR0A,UDRE
+    LDS TEMP0, UCSR0A
+    SBRS TEMP0, UDRE
+    RJMP UART_TRANSMIT
+
+    ; send the data
+    STS UDR0, TEMP
+    RET
