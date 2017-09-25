2cbdc7b7466ea65b7387ec59dbb3fbedd2ed96c5
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-09-25 18:20:15 -0400

    init project 1

---
+++ project/project1/main.cpp
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <gmp.h>
+
+/************************************************************/
+/* Complex type                                             */
+/************************************************************/
+
+struct complex_struct {
+  mpz_t r; // real part
+  mpz_t i; // imaginary part
+};
+
+typedef struct complex_struct complex;
+
+/************************************************************/
+/* Prototypes                                               */
+/************************************************************/
+
+// Complex four-multiply (four real multiplies)
+//   a, b - number to multiply
+//   z - return value (product)
+void cmul4(complex a, complex b, complex *z); 
+
+// Complex three-multiply (three real multiplies)
+//   a, b - number to multiply
+//   z - return value (product)
+void cmul3(complex a, complex b, complex *z); 
+
+// Complex list multiply using complex four-multiply
+//   u - array of complex
+//   first - index of first element to multiply
+//   last - index of last element to multiply
+//   z - return value, product of u[first..last]
+void cmul4_list(complex *u, int first, int last, complex *z);
+
+// Complex list multiply using complex three-multiply
+//   u, first, last, z - same as for cmul4_list()
+void cmul3_list(complex *u, int first, int last, complex *z);
+
+// Initialize a complex number; calls mpz_init()
+void init(complex * a);
+
+// Random complex value; real and imaginary parts at most 2^n
+//   z - complex number to be set to random values
+//   rstate - state variable for GMP random number generator
+//   n - number of bits in real and imaginary part
+void randz(complex *z, gmp_randstate_t rstate, mp_bitcnt_t n);
+
+/************************************************************/
+/* Main Program                                             */
+/************************************************************/
+
+int main() {
+  complex a, b, z;           // three complex variables
+  gmp_randstate_t rstate;    // state for GMP random number generator
+  clock_t t0, t;             // used for timing functions
+
+  // Initialize random number generator
+
+  gmp_randinit_default(rstate);
+
+  // Test of cmul4() and cmul3()
+
+  init(&a);
+  init(&b);
+  init(&z);
+
+  randz(&a, rstate, 20);
+  randz(&b, rstate, 20);
+
+  cmul4(a, b, &z);
+  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+
+  cmul3(a, b, &z);
+  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+
+  // Test and timing of cmul4_list() and cmul3_list()
+
+  // Generate array of random complex numbers
+
+  // You'll need to experiment with the length of the array (n) 
+  // and the size of the real and imaginary parts (third parameter 
+  // to randz).
+
+  int n = 10;                
+  complex v[n];
+  for (int i = 0; i < n; i++) {
+    init(&v[i]);
+    randz(&v[i], rstate, 200);
+  }
+
+  // Time call to cmul4_list()
+
+  t0 = clock();
+  cmul4_list(v, 0, n-1, &z);
+  t = clock() - t0;
+
+  fprintf(stdout, "*** CMUL4 List ***\n");
+  fprintf(stdout, "time: %f\n", ((double) t)/CLOCKS_PER_SEC);
+  //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+
+  // Time call to cmul3_list()
+
+  t0 = clock();
+  cmul3_list(v, 0, n-1, &z);
+  t = clock() - t0;
+
+  fprintf(stdout, "*** CMUL3 List ***\n");
+  fprintf(stdout, "time: %f\n", ((double) t)/CLOCKS_PER_SEC);
+  //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+
+  return 0;
+}
+
+/************************************************************/
+/* Function Implementations                                 */
+/************************************************************/
+
+//
+// TO BE WRITTEN: cmul4(), cmul3(), cmul4_list(), cmul3_list(),
+//                code to read data file
+//
+
+void randz(complex *z, gmp_randstate_t rstate, mp_bitcnt_t n) {
+  mpz_urandomb(z->r, rstate, n);
+  mpz_urandomb(z->i, rstate, n);
+}
+  
+void init(complex *a) {
+  mpz_init(a->r);
+  mpz_init(a->i);
+}
