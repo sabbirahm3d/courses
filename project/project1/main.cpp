149245804018029d52c3191e791972efe4dd4a5a
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-02 21:49:54 -0400

    add file I/O, add string to mpz_t manipulation

--- project/project1/main.cpp
+++ project/project1/main.cpp
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <math.h>
 #include <gmp.h>
+#include <string>
+#include <fstream>
+#include <iostream>
 #include <vector>
 
 /************************************************************/
@@ -54,68 +57,68 @@ void randz(complex *z, gmp_randstate_t rstate, mp_bitcnt_t n);
 /* Main Program                                             */
 /************************************************************/
 
-int main() {
-
-    complex a, b, z;           // three complex variables
-    gmp_randstate_t rstate;    // state for GMP random number generator
-    clock_t t0, t;             // used for timing functions
-
-    // Initialize random number generator
-
-    gmp_randinit_default(rstate);
-
-    // Test of cmul4() and cmul3()
-
-    init(&a);
-    init(&b);
-    init(&z);
-
-    randz(&a, rstate, 20);
-    randz(&b, rstate, 20);
-
-    cmul4(a, b, &z);
-    gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
-
-    cmul3(a, b, &z);
-    gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
-
-    // Test and timing of cmul4_list() and cmul3_list()
-
-    // Generate array of random complex numbers
-
-    // You'll need to experiment with the length of the array (n)
-    // and the size of the real and imaginary parts (third parameter
-    // to randz).
-
-    int n = 10;
-    complex v[n];
-    for (int i = 0; i < n; i++) {
-        init(&v[i]);
-        randz(&v[i], rstate, 200);
-    }
-
-    // Time call to cmul4_list()
-
-    t0 = clock();
-    cmul4_list(v, 0, n - 1, &z);
-    t = clock() - t0;
-
-    fprintf(stdout, "*** CMUL4 List ***\n");
-    fprintf(stdout, "time: %f\n", ((double) t) / CLOCKS_PER_SEC);
-    //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
-
-    // Time call to cmul3_list()
-
-    t0 = clock();
-    cmul3_list(v, 0, n - 1, &z);
-    t = clock() - t0;
-
-    fprintf(stdout, "*** CMUL3 List ***\n");
-    fprintf(stdout, "time: %f\n", ((double) t) / CLOCKS_PER_SEC);
-    //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
-
-    return 0;
-}
+//int main() {
+//
+//    complex a, b, z;           // three complex variables
+//    gmp_randstate_t rstate;    // state for GMP random number generator
+//    clock_t t0, t;             // used for timing functions
+//
+//    // Initialize random number generator
+//
+//    gmp_randinit_default(rstate);
+//
+//    // Test of cmul4() and cmul3()
+//
+//    init(&a);
+//    init(&b);
+//    init(&z);
+//
+//    randz(&a, rstate, 20);
+//    randz(&b, rstate, 20);
+//
+//    cmul4(a, b, &z);
+//    gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+//
+//    cmul3(a, b, &z);
+//    gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+//
+//    // Test and timing of cmul4_list() and cmul3_list()
+//
+//    // Generate array of random complex numbers
+//
+//    // pairu'll need to experiment with the length of the array (n)
+//    // and the size of the real and imaginary parts (third parameter
+//    // to randz).
+//
+//    int n = 10;
+//    complex v[n];
+//    for (int i = 0; i < n; i++) {
+//        init(&v[i]);
+//        randz(&v[i], rstate, 200);
+//    }
+//
+//    // Time call to cmul4_list()
+//
+//    t0 = clock();
+//    cmul4_list(v, 0, n - 1, &z);
+//    t = clock() - t0;
+//
+//    fprintf(stdout, "*** CMUL4 List ***\n");
+//    fprintf(stdout, "time: %f\n", ((double) t) / CLOCKS_PER_SEC);
+//    //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+//
+//    // Time call to cmul3_list()
+//
+//    t0 = clock();
+//    cmul3_list(v, 0, n - 1, &z);
+//    t = clock() - t0;
+//
+//    fprintf(stdout, "*** CMUL3 List ***\n");
+//    fprintf(stdout, "time: %f\n", ((double) t) / CLOCKS_PER_SEC);
+//    //  gmp_fprintf(stdout, "(%Zd, %Zd)\n", z.r, z.i);
+//
+//    return 0;
+//}
 
 ///************************************************************/
 ///* Function Implementations                                 */
@@ -130,29 +133,73 @@ int main() {
 //    mpz_urandomb(z->r, rstate, n);
 //    mpz_urandomb(z->i, rstate, n);
 //}
-//
-//void init(complex *a) {
-//    mpz_init(a->r);
-//    mpz_init(a->i);
-//}
-//
-//
-// TESTING GMP
-#include <iostream>
 
-int main() {
-    mpz_t result, base;
-    mpz_inits(result, base, NULL);
-    mpz_set_str(base, "2", 10);
-    mpz_pow_ui(result, base, 20000);
-    mpz_out_str(stdout, 10, result);
-    return 0;
+void init(complex *a) {
+    mpz_init(a->r);
+    mpz_init(a->i);
 }
 
-int main() {
+mpz_t* split_line(const std::string &str) {
 
-    string testFile = "cmplx_numbers_20_100bit.txt";
+    std::string next;
+    mpz_t pair[2];
+    size_t str_size;
 
-    readFromFiles();
+    mpz_init(pair[0]);
+    mpz_init(pair[1]);
+
+    // for each char
+    for (char it : str) {
+        // if the char == ','
+        if (it == ',') {
+            // If we have some characters accumulated
+            if (!next.empty()) {
+                str_size = next.size();
+                mpz_set_str(pair[0], next.substr(1, str_size).c_str(), 10);
+                gmp_printf("%s is an mpz %Zd\n", "here", pair[0]);
+                next.clear();
+            }
+        } else {
+            // Accumulate the next character into the sequence
+            next += it;
+        }
+    }
+
+    if (!next.empty()) {
+        str_size = next.size();
+        mpz_set_str(pair[1], next.substr(0, str_size - 1).c_str(), 10);
+        gmp_printf("%s is an mpz %Zd\n", "here", pair[1]);
+    }
+
+    return pair;
+
+}
+
+
+void read_from_file(const char *file_name) {
+
+    std::ifstream file(file_name);
+    std::string line;
+    std::vector<complex> complex_nums;
+
+    while (std::getline(file, line)) {
+        split_line(line);
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc == 2) {
+
+        read_from_file(argv[1]);
+        return 0;
+
+    } else {
+
+        std::cout << "Please provide a valid file name" << std::endl;
+        return -1;
+
+    }
 
 }
