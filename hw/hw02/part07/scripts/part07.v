6090c6d2b95ab24d3a6a7f91a0c44bf80e12bf7c
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-06 18:01:38 -0400

    restructure latex files, finish part06 report

---
+++ hw/hw02/part07/scripts/part07.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:53:38 10/03/2017 
+// Design Name: 
+// Module Name:    part06 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module part07(a, b, clk, y, z);
+
+    input a, b, clk;
+    output reg [1:0] y, z;
+
+    always @(posedge clk) begin
+        y = 2'b00;
+        $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+        if (a == 1) begin
+            y[1] = 1'b1;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+            y[0] = 1'bx;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+            z = 2'b01;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+        end else if (b == 1) begin
+            y[0] = 1'b0;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+            y[1] = 1'b0;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+            z = 2'b10;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
+        end
+    end
+
+endmodule
--- hw/hw02/part08/scripts/part08.v
+++ hw/hw02/part08/scripts/part08.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    14:13:00 10/05/2017 
+// Create Date:    17:53:38 10/03/2017 
 // Design Name: 
-// Module Name:    part08 
+// Module Name:    part06 
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -24,15 +24,22 @@ module part08(a, b, clk, y, z);
     output reg [1:0] y, z;
 
     always @(posedge clk) begin
-        y <= 2'b00;
+        y = 2'b00;
+        $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
         if (a == 1) begin
             y[1] <= 1'b1;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
             y[0] <= 1'bx;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
             z <= 2'b01;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
         end else if (b == 1) begin
             y[0] <= 1'b0;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
             y[1] <= 1'b0;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
             z <= 2'b10;
+            $display("%0t    %b   %b   %b   %b", $time, a, b, y, z);
         end
     end
 
