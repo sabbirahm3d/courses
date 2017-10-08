24b8f438cde6ddd6f39cc2eb6f2a15fbd7e6c75d
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-08 13:29:07 -0400

    add packing and unpacking notes funcs

--- hw/hw02/code/music.c
+++ hw/hw02/code/music.c
@@ -1,7 +1,79 @@
 #include "music.h"
 
+
+uint8_t *int2bin(int a, uint8_t *buffer) {
+
+    buffer += (7);
+
+    for (int i = 7; i >= 0; i--) {
+
+        *buffer-- = (a & 1) + '0';
+        a >>= 1;
+
+    }
+
+    return buffer;
+}
+
+uint8_t unpack_note_letter_ascii(uint8_t packed_note) {
+
+    return packed_note >> 5;
+
+}
+
+uint8_t unpack_note_duration(uint8_t packed_note) {
+
+    return (uint8_t) (packed_note & 0x1F);
+
+}
+
+
+uint8_t pack_note(char letter_ascii, uint8_t duration) {
+
+    static uint8_t buffer[9];
+    buffer[8] = '\0';
+    int2bin((letter_ascii << 5) + duration, buffer);
+    printf("b = %s\n", buffer);
+
+    uint8_t a = (uint8_t) (letter_ascii << 5) + duration;
+    printf("d = %d\n", a);
+
+    return a;
+
+}
+
+
 int main() {
 
+    uint8_t buffer, temp;
+
+    buffer = pack_note(NOTE_B, 2);
+    printf("B2 = %d\n", buffer);
+    temp = unpack_note_letter_ascii(buffer);
+    printf("B = %d\n", temp);
+    temp = unpack_note_duration(buffer);
+    printf("2 = %d\n", temp);
+
+    buffer = pack_note(NOTE_A, 2);
+    printf("A2 = %d\n", buffer);
+    temp = unpack_note_letter_ascii(buffer);
+    printf("A = %d\n", temp);
+    temp = unpack_note_duration(buffer);
+    printf("2 = %d\n", temp);
+
+    buffer = pack_note(NOTE_R, 1);
+    printf("R1 = %d\n", buffer);
+    temp = unpack_note_letter_ascii(buffer);
+    printf("R = %d\n", temp);
+    temp = unpack_note_duration(buffer);
+    printf("1 = %d\n", temp);
+
+    buffer = pack_note(NOTE_C, 2);
+    printf("C2 = %d\n", buffer);
+    temp = unpack_note_letter_ascii(buffer);
+    printf("C = %d\n", temp);
+    temp = unpack_note_duration(buffer);
+    printf("2 = %d\n", temp);
 
     return 0;
 
--- hw/hw02/code/music.h
+++ hw/hw02/code/music.h
@@ -13,13 +13,42 @@
 #include <string.h>
 // #include "U0_UART.h"  // to direct standard I/O to AVR UART
 
+#define PORTB5_SPEAKER_MASK 0b00100000
+#define USER_LINE_MAX 128
+char userLine[USER_LINE_MAX];
+#define NUMBER_OF_SONGS 4
+#define MAX_SONG_LENGTH 64
+#define NOTE_A 0
+#define NOTE_B 1
+#define NOTE_C 2
+#define NOTE_D 3
+#define NOTE_E 4
+#define NOTE_F 5
+#define NOTE_G 6
+#define NOTE_R 7
 
-uint8_t PackNote (char letterASCII, uint8_t duration);
-uint8_t UnpackNoteLetterASCII
-        (uint8_t packedNote);
-uint8_t UnpackNoteDuration (uint8_t packedNote);
-void StoreSong(uint8_t song[],const char songString[]);
+//char songTitle[NUMBER_OF_SONGS][STR_LENGTH] = {"Title1", "Title2", "Title3", "Title4"};
+char song[NUMBER_OF_SONGS][MAX_SONG_LENGTH] = {
+        {(NOTE_B << 5) + 2, (NOTE_A << 5) + 2, (NOTE_G << 5) + 2},
+        {NOTE_R << 5},
+        {NOTE_R << 5},
+        {NOTE_R << 5}
+};
+//// some initial song with notes
+const char menuMain[] = "Main Menu\nCreate Song\nPlay Song\nList Songs\n";
+const char
+        menuPlay[] = "Play Menu\nSearch By Title\nNumber\n";
+
+uint8_t pack_note(char, uint8_t);
+
+uint8_t unpack_note_letter_ascii(uint8_t);
+
+uint8_t unpack_note_duration(uint8_t);
+
+void store_songs(uint8_t song[], const char songString[]);
+
 void PlayNote(uint8_t letterASCII, uint8_t quarters);
+
 void PlaySong(uint8_t song[]);
 
 #endif //HW02_MAIN_H
