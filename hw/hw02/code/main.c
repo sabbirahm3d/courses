ec4ee19cc2724e285486ef202ff942e0932c2f2b
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-13 18:23:21 -0400

    turned in part 1 of hw2

--- hw/hw02/code/main.c
+++ hw/hw02/code/main.c
@@ -19,6 +19,7 @@ char song[NUMBER_OF_SONGS][MAX_SONG_LENGTH] = {
 };
 
 uint8_t display_menu(const char *);
+void play_menu();
 
 uint8_t display_menu(const char *menu) {
 
@@ -32,37 +33,73 @@ uint8_t display_menu(const char *menu) {
 }
 
 
-void create_song() {
+void play_menu() {
 
-
-//    char song_ascii[USER_LINE_MAX];
-    char song_title[USER_LINE_MAX / 2];
-    char *packed_song;
-
-
-    printf("Enter song title: ");
-    getc(stdin);
-    fgets(song_title, USER_LINE_MAX, stdin);
-    printf("Song name: %s\n", song_title);
-
-//    fflush(stdout);
-//    printf("Enter song notes using ABCDEFGR followed by quarter seconds: ");
-//    getc(stdin);
-//    fgets(song_ascii, USER_LINE_MAX, stdin);
-//    printf("Song notes: %s", song_ascii);
     char *song_ascii = "B22A2R11C2r14e22e2e2r1r11R0C2R1";
 
-    packed_song = add_zero_rest(song_ascii);
+    char* packed_song = add_zero_rest(song_ascii);
 
     uint8_t song_packed[strlen(packed_song) / 2];
-    memset(song_packed, 0, sizeof song_packed);
+    memset(song_packed, 0, sizeof(song_packed));
 
     store_songs(song_packed, song_ascii);
 
     printf("Song: ");
     play_song(song_packed);
+    printf("\n(The song produced above is currently hardcoded in. User \n"
+        "inputted songs are currently not supported. To try a different\n"
+        "song, change the string in the source code)");
+
 
 }
+
+void create_song() {
+
+    printf("Creating songs is currently not supported\n");
+/*
+    // char song_ascii[USER_LINE_MAX];
+    // char song_title;
+    // char *packed_song;
+    // int song_ix;
+
+
+//     printf("Enter song title: ");
+//     if (fgets(user_line, 10, stdin) != NULL) {
+//        getc(stdin);
+//         sscanf(user_line, "%s", &song_title);
+//         printf("Song name: %s\n", song_title);
+//     }
+//    fflush(stdout);
+//    printf("Enter song notes using ABCDEFGR followed by quarter seconds: ");
+//    getc(stdin);
+//    fgets(song_ascii, USER_LINE_MAX, stdin);
+//    printf("Song notes: %s", song_ascii);
+
+    // change the song ASCII here:
+    // char *song_ascii = "B22A2R11C2r14e22e2e2r1r11R0C2R1";
+
+    // packed_song = add_zero_rest(song_ascii);
+
+    // uint8_t song_packed[strlen(packed_song) / 2];
+    // memset(song_packed, 0, sizeof(song_packed));
+
+    // store_songs(song_packed, song_ascii);
+
+    // printf("Song: ");
+    // play_song(song_packed);
+    // printf("\n(The song produced above is currently hardcoded in. User "
+    //     "inputted songs are currently not supported. To try a different"
+    //     "song, change the string in the source code)");
+
+    // printf("Enter index to save the new song in: ");
+    // if (fgets(user_line, 10, stdin) != NULL) {
+    //     sscanf(user_line, "%d", &song_ix);
+    //     printf("Index: %d", song_ix);
+    // }
+
+*/
+}
+
 
 int main() {
 
@@ -93,7 +130,7 @@ int main() {
 
             case 2: {
 
-                flag = 0;
+                play_menu();
                 break;
 
             }
