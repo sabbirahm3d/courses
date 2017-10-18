3f67b13bfc7757c070d92c4e5fac13b22ba2f2f6
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-17 21:46:23 -0400

    add plots and plotting scripts

--- project/project1/analysis/plot.py
+++ project/project1/analysis/plot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from sys import argv
 from ast import literal_eval
+from glob import glob
 
 import plotly.plotly as py
 import plotly.graph_objs as go
@@ -10,15 +10,69 @@ import plotly.graph_objs as go
 from secret import API_KEY, USERNAME
 
 
-def diff_scatter(x, y, title):
+def dist_scatter(x, cmul4_line, cmul3_line):
 
-    trace = go.Scatter(
+    cmul4 = go.Scatter(
         x=x,
-        y=y,
-        mode="markers",
+        y=cmul4_line,
+        name="cmul4()",  # Style name/legend entry with html tags
+        connectgaps=True
     )
-    data = [trace]
-    layout = go.Layout(title=title, height=900, width=2000)
+    cmul3 = go.Scatter(
+        x=x,
+        y=cmul3_line,
+        name="cmul3()",
+    )
+
+    data = [cmul4, cmul3]
+
+    # fig = dict(data=data)
+
+    layout = go.Layout(
+        title="Mean Runtimes",
+        xaxis={
+            "title": "Data Size",
+        },
+        yaxis={
+            "title": "ms",
+        },
+        font={"size": 18},
+        height=900, width=1600
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    return fig
+
+    # py.iplot(fig, filename="simple-connectgaps")
+
+
+def diff_boxplot(file_names, diffs):
+
+    data = []
+    for file_name, diff in zip(file_names, diffs):
+        data.append(go.Box(
+            y=diff,
+            name=file_name[16:],
+            boxpoints="all",
+            jitter=0.2,
+            whiskerwidth=0.2,
+            line=dict(width=1),
+            showlegend=False,
+        ))
+
+    layout = go.Layout(
+        title="Distribution of Differences in The Multiplication Methods",
+        xaxis={
+            "title": "Data Size",
+        },
+        yaxis={
+            "title": "$(t_{cmul4()} - t_{cmul3()}) \ ms$",
+        },
+        font={"size": 18},
+        height=900, width=1600
+    )
+
     fig = go.Figure(data=data, layout=layout)
 
     return fig
@@ -26,20 +80,34 @@ def diff_scatter(x, y, title):
 
 if __name__ == "__main__":
 
-    if len(argv) > 1:
+    times_dir = "times/*bit.txt"
+    fig_dir = "report/figures/"
 
-        file_name = argv[-1].split('/')[-1].replace(".txt", ".png")
-        data = []
-        with open(argv[-1]) as f:
-            for _ in range(6):
-                next(f)  # quick way to skip the lines
-            for j in f:
-                data.append(j)
+    times_files = sorted(glob(times_dir))
 
-        data = literal_eval(data[1])
+    file_names = [x.split("/")[-1][:-4] for x in times_files]
+    means = []
+    medians = []
+    std_devs = []
+    diffs = []
 
-        # credentials for plot.ly API
-        py.sign_in(username=USERNAME, api_key=API_KEY)
+    for file in times_files:
+        with open(file) as anaylsis_file:
+            for _ in range(2):
+                means.append(float(next(anaylsis_file)[11:-4]))
+            for _ in range(2):
+                medians.append(float(next(anaylsis_file)[11:-4]))
+            for _ in range(2):
+                std_devs.append(float(next(anaylsis_file)[11:-4]))
 
-        fig = diff_scatter(list(range(1, 201)), data, file_name[:-4])
-        py.image.save_as(fig, filename=file_name)
+            next(anaylsis_file)
+            diffs.append(literal_eval(next(anaylsis_file)))
+
+    # credentials for plot.ly API
+    py.sign_in(username=USERNAME, api_key=API_KEY)
+
+    fig = diff_boxplot(file_names, diffs)
+    py.image.save_as(fig, filename=fig_dir + "diffs.png")
+
+    fig = dist_scatter([x[16:] for x in file_names], means[::2], means[1::2])
+    py.image.save_as(fig, filename=fig_dir + "means.png")
