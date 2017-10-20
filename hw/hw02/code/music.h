3846a73a88193628489a5b19fb04d35d76773046
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-20 18:55:07 -0400

    finish documentation on hw02

--- hw/hw02/code/music.h
+++ hw/hw02/code/music.h
@@ -1,13 +1,12 @@
-//
-// Created by sabbir on 10/8/17.
-//
+/*
+ * music.h
+ *
+ * This file contains the declarations for the functions in music.c and main.c
+ *
+ * */
 
 #ifndef MUSIC_H
 #define MUSIC_H
-
-#include <avr/io.h>
-#include <avr/pgmspace.h>
-#include <util/delay.h>
 
 #include <ctype.h>
 #include <inttypes.h>
@@ -15,17 +14,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "U0_UART.h"  // to direct standard I/O to AVR UART
+// AVR libraries
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+// standard I/O to the AVR UART
+#include "U0_UART.h"
 
 extern FILE uart_stream;
 
+// PORTB pin 5
 #define PORTB5_SPEAKER_MASK 0b00100000
 
+// constraints on sizes of strings
 #define USER_LINE_MAX 128
 #define NUMBER_OF_SONGS 4
 #define MAX_SONG_LENGTH 64
 #define MAX_TITLE_LENGTH 30
 
+// ASCII character notes mapped to their assigned values
 #define NOTE_A 0
 #define NOTE_B 1
 #define NOTE_C 2
@@ -36,6 +44,7 @@ extern FILE uart_stream;
 #define NOTE_R 7
 #define ZERO_REST (NOTE_R << 5)  // R0
 
+// frequencies mapped to individual ASCII notes
 #define FREQ_A 440
 #define FREQ_B 494
 #define FREQ_C 262
@@ -44,11 +53,12 @@ extern FILE uart_stream;
 #define FREQ_F 350
 #define FREQ_G 392
 #define FREQ_R 4
-#define FREQ_TO_MS 500000
+#define FREQ_TO_MS 500000  // required for computing the number of iterations
 
-
+// holds user input
 char user_line[USER_LINE_MAX];
 
+// -------------------------- HELPER FUNCTIONS --------------------------
 char *add_zero_rest(const char[]);
 
 void delay_ms(int);
