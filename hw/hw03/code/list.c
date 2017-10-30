39421f353f76d9050d921cd67a13e195a9f63410
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-10-29 22:55:45 -0400

    major update to hw03

--- hw/hw03/code/list.c
+++ hw/hw03/code/list.c
@@ -5,6 +5,8 @@
 // Copyright (c) 2010 TJ Holowaychuk <tj@vision-media.ca>
 //
 
+#include <string.h>
+#include <stdio.h>
 #include "list.h"
 
 /*
@@ -12,7 +14,7 @@
  */
 
 
-node *create_node(char **data, float grade) {
+node *create_node(char **data, float list_of_grades, char *grade) {
 
     node *self;
     if (!(self = malloc(sizeof(node))))
@@ -21,6 +23,7 @@ node *create_node(char **data, float grade) {
     self->next = NULL;
     self->name = *data;
     self->grade = grade;
+    self->list_of_grades = list_of_grades;
 
     return self;
 
@@ -31,30 +34,16 @@ node *create_node(char **data, float grade) {
  * Accepts a direction, which may be LIST_HEAD or LIST_TAIL.
  */
 
-list_iterator_t *
-list_iterator_new(linked_list *list, list_direction_t direction) {
-
-    node *node = direction == LIST_HEAD ? list->head : list->tail;
-    return list_iterator_new_from_node(node, direction);
-
-}
-
-/*
- * Allocate a new list_iterator_t with the given start
- * node. NULL on failure.
- */
-
-list_iterator_t *
-list_iterator_new_from_node(node *node, list_direction_t direction) {
+list_iterator_t *list_iterator_new(linked_list *list) {
 
     list_iterator_t *self;
     if (!(self = malloc(sizeof(list_iterator_t))))
         return NULL;
-    self->next = node;
-    self->direction = direction;
+    self->next = list->head;
     return self;
 
 }
+
 
 /*
  * Return the next node or NULL when no more
@@ -64,9 +53,7 @@ list_iterator_new_from_node(node *node, list_direction_t direction) {
 node *list_iterator_next(list_iterator_t *self) {
     node *curr = self->next;
     if (curr) {
-        self->next = self->direction == LIST_HEAD
-                ? curr->next
-                : curr->prev;
+        self->next = curr->next;
     }
     return curr;
 }
@@ -120,147 +107,164 @@ void list_destroy(linked_list *self) {
     free(self);
 }
 
+int compare(node *node1, node *node2) {
+    return strcmp(node1->name, node2->name);
+}
+
 /*
  * Append the given node to the list
  * and return the node, NULL on failure.
  */
 
 node *list_push(linked_list *self, node *node) {
-    if (!node) return NULL;
+
+    if (!node) {
+        return NULL;
+    }
 
     if (self->len) {
+
+//        for (; node != NULL; node = node->next) {
+//            if (compare(node, node->next) > 0) break;
+//        }
         node->prev = self->tail;
         node->next = NULL;
         self->tail->next = node;
         self->tail = node;
+
+        printf("COMPARE %d", compare(node, self->head));
+
     } else {
+
         self->head = self->tail = node;
         node->prev = node->next = NULL;
+
     }
 
     ++self->len;
     return node;
 }
+//
+///*
+// * Return / detach the last node in the list, or NULL.
+// */
+//
+//node *list_rpop(linked_list *self) {
+//    if (!self->len) return NULL;
+//
+//    node *node = self->tail;
+//
+//    if (--self->len) {
+//        (self->tail = node->prev)->next = NULL;
+//    } else {
+//        self->tail = self->head = NULL;
+//    }
+//
+//    node->next = node->prev = NULL;
+//    return node;
+//}
+//
+///*
+// * Return / detach the first node in the list, or NULL.
+// */
+//
+//node *list_lpop(linked_list *self) {
+//    if (!self->len) return NULL;
+//
+//    node *node = self->head;
+//
+//    if (--self->len) {
+//        (self->head = node->next)->prev = NULL;
+//    } else {
+//        self->head = self->tail = NULL;
+//    }
+//
+//    node->next = node->prev = NULL;
+//    return node;
+//}
 
-/*
- * Return / detach the last node in the list, or NULL.
- */
-
-node *list_rpop(linked_list *self) {
-    if (!self->len) return NULL;
-
-    node *node = self->tail;
-
-    if (--self->len) {
-        (self->tail = node->prev)->next = NULL;
-    } else {
-        self->tail = self->head = NULL;
-    }
-
-    node->next = node->prev = NULL;
-    return node;
-}
-
-/*
- * Return / detach the first node in the list, or NULL.
- */
-
-node *list_lpop(linked_list *self) {
-    if (!self->len) return NULL;
-
-    node *node = self->head;
-
-    if (--self->len) {
-        (self->head = node->next)->prev = NULL;
-    } else {
-        self->head = self->tail = NULL;
-    }
-
-    node->next = node->prev = NULL;
-    return node;
-}
-
-/*
- * Prepend the given node to the list
- * and return the node, NULL on failure.
- */
-
-node *list_lpush(linked_list *self, node *node) {
-    if (!node) return NULL;
-
-    if (self->len) {
-        node->next = self->head;
-        node->prev = NULL;
-        self->head->prev = node;
-        self->head = node;
-    } else {
-        self->head = self->tail = node;
-        node->prev = node->next = NULL;
-    }
-
-    ++self->len;
-    return node;
-}
-
-/*
- * Return the node associated to name or NULL.
- */
-
-node *list_find(linked_list *self, char *name) {
-    list_iterator_t *it = list_iterator_new(self, LIST_HEAD);
-    node *node;
-
-    while ((node = list_iterator_next(it))) {
-
-        if (self->match) {
-            if (self->match(name, node->name)) {
-                list_iterator_destroy(it);
-                return node;
-            }
-
-        } else {
-
-            if (name == node->name) {
-                list_iterator_destroy(it);
-                return node;
-            }
-
-        }
-
-    }
-
-    list_iterator_destroy(it);
-    return NULL;
-}
-
-/*
- * Return the node at the given index or NULL.
- */
-
-node *list_get(linked_list *self, int index) {
-    list_direction_t direction = LIST_HEAD;
-
-    if (index < 0) {
-        direction = LIST_TAIL;
-        index = ~index;
-    }
-
-    if ((unsigned) index < self->len) {
-        list_iterator_t *it = list_iterator_new(self, direction);
-        node *node = list_iterator_next(it);
-        while (index--) node = list_iterator_next(it);
-        list_iterator_destroy(it);
-        return node;
-    }
-
-    return NULL;
-}
+///*
+// * Prepend the given node to the list
+// * and return the node, NULL on failure.
+// */
+//
+//node *list_lpush(linked_list *self, node *node) {
+//    if (!node) return NULL;
+//
+//    if (self->len) {
+//        node->next = self->head;
+//        node->prev = NULL;
+//        self->head->prev = node;
+//        self->head = node;
+//    } else {
+//        self->head = self->tail = node;
+//        node->prev = node->next = NULL;
+//    }
+//
+//    ++self->len;
+//    return node;
+//}
+//
+///*
+// * Return the node associated to name or NULL.
+// */
+//
+//node *list_find(linked_list *self, char *name) {
+//    list_iterator_t *it = list_iterator_new(self, LIST_HEAD);
+//    node *node;
+//
+//    while ((node = list_iterator_next(it))) {
+//
+//        if (self->match) {
+//            if (self->match(name, node->name)) {
+//                list_iterator_destroy(it);
+//                return node;
+//            }
+//
+//        } else {
+//
+//            if (name == node->name) {
+//                list_iterator_destroy(it);
+//                return node;
+//            }
+//
+//        }
+//
+//    }
+//
+//    list_iterator_destroy(it);
+//    return NULL;
+//}
+//
+///*
+// * Return the node at the given index or NULL.
+// */
+//
+//node *list_get(linked_list *self, int index) {
+//    list_direction_t direction = LIST_HEAD;
+//
+//    if (index < 0) {
+//        direction = LIST_TAIL;
+//        index = ~index;
+//    }
+//
+//    if ((unsigned) index < self->len) {
+//        list_iterator_t *it = list_iterator_new(self, direction);
+//        node *node = list_iterator_next(it);
+//        while (index--) node = list_iterator_next(it);
+//        list_iterator_destroy(it);
+//        return node;
+//    }
+//
+//    return NULL;
+//}
 
 /*
  * Remove the given node from the list, freeing it and it's value.
  */
 
 void list_remove(linked_list *self, node *node) {
+
     node->prev ? (node->prev->next = node->next) : (self->head = node->next);
 
     node->next ? (node->next->prev = node->prev) : (self->tail = node->prev);
@@ -268,6 +272,8 @@ void list_remove(linked_list *self, node *node) {
     if (self->free) self->free(node->name);
 
     free(node->name);
+    free(node->grade);
     free(node);
     --self->len;
+
 }
--- hw/hw03/code/main.c
+++ hw/hw03/code/main.c
@@ -4,7 +4,7 @@
 #include "list.h"
 
 const char menu_main_str[] = "---------- MAIN MENU ----------\n1. "
-        "List Database\n2. Search by Name\n3. Search by Grade\n4. Add "
+        "List Database\n2. Search by Name\n3. Search by list_of_grades\n4. Add "
         "Student\n5. Remove Student\n0. Exit\nChoose: ";
 char user_line[20];
 
@@ -28,31 +28,34 @@ int display_menu() {
 
 }
 
-void init_db(linked_list *database) {
-
-    char *name = "test";
-    float grade = 92.1;
-    list_push(database, create_node(&name, grade));
-
-}
+//void init_db(linked_list *database) {
+//
+//    char *name = "test";
+//    float list_of_grades = 92.1;
+//    list_push(database, create_node(&name, list_of_grades));
+//
+//}
 
 void print_db(linked_list *database) {
 
     if (database->len) {
 
         node *temp;
-        list_iterator_t *it = list_iterator_new(database, LIST_HEAD);
+        list_iterator_t *it = list_iterator_new(database);
 
         while ((temp = list_iterator_next(it))) {
-            printf("%s: %.2f%%\n", temp->name, temp->grade);
+            printf("%s: %.2f%% %c\n", temp->name, temp->list_of_grades,
+                   *temp->grade);
         }
 
         free(temp);
         list_iterator_destroy(it);
-    } else {
-        printf("Database is empty!\n");
-    }
 
+    } else {
+
+        printf("Database is empty!\n");
+
+    }
     printf("\n");
 
 }
@@ -69,12 +72,12 @@ void search_name(linked_list *database) {
     }
 
     node *temp;
-    list_iterator_t *it = list_iterator_new(database, LIST_HEAD);
+    list_iterator_t *it = list_iterator_new(database);
 
     while ((temp = list_iterator_next(it))) {
         if (!strcasecmp(temp->name, name)) {
             printf("Found student: %s\n", temp->name);
-            printf("Student data: %.2f%%\n", temp->grade);
+            printf("Student data: %.2f%%\n", temp->list_of_grades);
             found = 1;
         }
     }
@@ -94,19 +97,26 @@ void search_name(linked_list *database) {
 void add_data(linked_list *database) {
 
     char *name = malloc(20 * sizeof(char));
+    float list_of_grades;
+    char *grade = malloc(1 * sizeof(char));
+
     printf("Enter name: ");
     if (fgets(user_line, 20, stdin) != NULL) {
         sscanf(user_line, "%[^\n]", name);
     }
 
-    // prompt user for grade
-    float grade;
-    printf("Enter grade: ");
+    // prompt user for list_of_grades
+    printf("Enter list_of_grades: ");
     if (fgets(user_line, 20, stdin) != NULL) {
-        sscanf(user_line, "%f", &grade);
+        sscanf(user_line, "%f", &list_of_grades);
     }
 
-    list_push(database, create_node(&name, grade));
+    printf("Enter grade: ");
+    if (fgets(user_line, 20, stdin) != NULL) {
+        sscanf(user_line, "%c", grade);
+    }
+
+    list_push(database, create_node(&name, list_of_grades, grade));
     printf("\n");
 
 }
@@ -120,7 +130,7 @@ void remove_data(linked_list *database) {
     }
 
     node *temp;
-    list_iterator_t *it = list_iterator_new(database, LIST_HEAD);
+    list_iterator_t *it = list_iterator_new(database);
 
     while ((temp = list_iterator_next(it))) {
         if (!strcasecmp(temp->name, name)) {
@@ -140,10 +150,11 @@ void remove_data(linked_list *database) {
 void close_db(linked_list *database) {
 
     node *temp;
-    list_iterator_t *it = list_iterator_new(database, LIST_HEAD);
+    list_iterator_t *it = list_iterator_new(database);
 
     while ((temp = list_iterator_next(it))) {
         free(temp->name);
+        free(temp->grade);
     }
     free(temp);
 
@@ -157,9 +168,26 @@ int main(void) {
 
     linked_list *database = list_new();
 //    init_db(database);
-
     int choice;
     int loop_flag = 1;
+
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+
+    fp = fopen("input.txt", "r");
+
+    if (fp == NULL)
+        return EXIT_FAILURE;
+
+    while (getline(&line, &len, fp) != -1) {
+        printf("%s\n", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
 
     while (loop_flag) {
 
@@ -167,40 +195,60 @@ int main(void) {
 
         switch (choice) {
 
-            case 1: {  // print out the database
+            case 1: {
+
+                // print out the database
                 print_db(database);
                 break;
+
             }
 
-            case 2: {  // search database by name
+            case 2: {
+
+                // search database by name
                 search_name(database);
                 break;
+
             }
 
-            case 3: {  // search database by grade
+            case 3: {
+
+                // search database by list_of_grades
 
                 break;
             }
 
-            case 4: {  // add student data to database
+            case 4: {
+
+                // add student data to database
                 add_data(database);
                 break;
+
             }
 
-            case 5: {  // remove student data from database
+            case 5: {
+
+                // remove student data from database
                 remove_data(database);
                 break;
+
             }
 
-            case 0: {  // terminate program
+            case 0: {
+
+                // terminate program
                 loop_flag = 0;
                 close_db(database);
                 break;
+
             }
 
             default: {
+
                 printf("\033[H\033[J");  // to clear screen
+                printf("Please enter a valid option.\n");
                 break;
+
             }
 
         }
