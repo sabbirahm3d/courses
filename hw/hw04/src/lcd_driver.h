9e54733a4524bad0f52f9d6fddf3e5820999327e
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-11-18 16:38:56 -0500

    init hw 4

---
+++ hw/hw04/src/lcd_driver.h
@@ -0,0 +1,29 @@
+//
+// Created by sabbir on 11/18/17.
+//
+
+#ifndef LCD_DRIVER_H
+#define LCD_DRIVER_H
+
+#include <avr/io.h>
+
+#define LCD_REGISTER_COUNT 20
+
+#define LCD_INITIAL_CONTRAST 0x0F
+
+#define FALSE 0
+#define TRUE (!FALSE)
+
+// LCD Data Register 0    // DEVICE SPECIFIC!!! (ATmega169)
+#define pLCDREG ((unsigned char *)(0xEC))
+
+// DEVICE SPECIFIC!!! (ATmega169) First LCD segment register
+#define LCD_CONTRAST_LEVEL(level) LCDCCR=((LCDCCR&0xF0)|(0x0F & level))
+
+//Functions
+void LCD_Init(void);
+
+void LCD_WriteDigit(char input, char digit);  //digit can be 0 to 5
+void LCD_AllSegments(char show);
+
+#endif // LCD_DRIVER_H
