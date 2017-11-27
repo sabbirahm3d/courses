7a704b50bbe1c552e2622ea597329a1b3967faa3
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-11-27 18:41:22 -0500

    done refactoring, begin documentation

--- hw/hw04/src/main.c
+++ hw/hw04/src/main.c
@@ -8,8 +8,7 @@
  */
 
 // standard libraries
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 
 // AVR libraries
 #include <avr/io.h>
@@ -73,12 +72,6 @@ uint8_t INITADC;
 // Look-up table used when converting ASCII to
 // LCD display data (segment control)
 unsigned int LCDCHARTABLE[] = {
-        0x0A51,     // '*' (?)
-        0x2A80,     // '+'
-        0x0000,     // ',' (Not defined)
-        0x0A00,     // '-'
-        0x0A51,     // '.' Degree sign
-        0x0000,     // '/' (Not defined)
         0x5559,     // '0'
         0x0118,     // '1'
         0x1e11,     // '2
@@ -89,44 +82,10 @@ unsigned int LCDCHARTABLE[] = {
         0x0111,     // '7
         0x1f51,     // '8
         0x1b51,     // '9'
-        0x0000,     // ':' (Not defined)
-        0x0000,     // ';' (Not defined)
-        0x0000,     // '<' (Not defined)
-        0x0000,     // '=' (Not defined)
-        0x0000,     // '>' (Not defined)
-        0x0000,     // '?' (Not defined)
-        0x0000,     // '@' (Not defined)
         0x0f51,     // 'A' (+ 'a')
-        0x3991,     // 'B' (+ 'b')
-        0x1441,     // 'C' (+ 'c')
-        0x3191,     // 'D' (+ 'd')
-        0x1e41,     // 'E' (+ 'e')
         0x0e41,     // 'F' (+ 'f')
-        0x1d41,     // 'G' (+ 'g')
-        0x0f50,     // 'H' (+ 'h')
-        0x2080,     // 'I' (+ 'i')
-        0x1510,     // 'J' (+ 'j')
-        0x8648,     // 'K' (+ 'k')
         0x1440,     // 'L' (+ 'l')
-        0x0578,     // 'M' (+ 'm')
-        0x8570,     // 'N' (+ 'n')
-        0x1551,     // 'O' (+ 'o')
-        0x0e51,     // 'P' (+ 'p')
-        0x9551,     // 'Q' (+ 'q')
-        0x8e51,     // 'R' (+ 'r')
-        0x9021,     // 'S' (+ 's')
-        0x2081,     // 'T' (+ 't')
-        0x1550,     // 'U' (+ 'u')
-        0x4448,     // 'V' (+ 'v')
-        0xc550,     // 'W' (+ 'w')
-        0xc028,     // 'X' (+ 'x')
-        0x2028,     // 'Y' (+ 'y')
-        0x5009,     // 'Z' (+ 'z')
-        0x0000,     // '[' (Not defined)
-        0x0000,     // '\' (Not defined)
-        0x0000,     // ']' (Not defined)
-        0x0000,     // '^' (Not defined)
-        0x0000      // '_'
+        0x2081     // 'T' (+ 't')
 };
 
 
@@ -483,6 +442,7 @@ int main() {
     adc_init();
     pins_init();
 
+	lcd_puts_P(FLTSTR);
     uint8_t mode = 0;
 
     while (!press_right()) {
