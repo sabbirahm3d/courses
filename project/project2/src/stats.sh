ea116bdd289d6674ae9334e40b690b55b16c697d
sabbirahm3d <sabbir0ahmed0@gmail.com>
2017-12-06 12:24:59 -0500

    fix stat generator

--- project/project2/src/stats.sh
+++ project/project2/src/stats.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -o errexit
 
 cores=( 1 2 4 8 16 )
-seq1=( 500 1000 2500 5000 10000 25000 )
-seq2=( 500 1000 2500 5000 10000 25000 )
+seqlens=( 500 1000 2500 5000 10000 25000 )
 
-for (( i = 1; i <= ${#cores[@]}; i++))
+for (( i = 0; i < ${#cores[@]}; i++))
 do
-    for (( j = 1; j <= ${#seq1[@]}; j++))
+    for (( j = 0; j < ${#seqlens[@]}; j++))
     do
 
-        for (( k = 1; k <= j; k++))
+        for (( k = 0; k <= j; k++))
         do
 
-            echo "CORES:\t$cores[i]\tLEN1:\t$seq1[k]\tLEN2:\t$seq2[j]";
+            echo "CORES: ${cores[i]} LEN1: ${seqlens[j]} LEN2: ${seqlens[k]}";
             for (( l = 1; l <= 5; l++))
             do
-                make -s run SEQ1=$seq1[k] SEQ2=$seq2[j] THREADS=$cores[i];
+                make run SEQ1LEN=${seqlens[j]} SEQ2LEN=${seqlens[k]} THREADS=${cores[i]}
             done
 
         done
