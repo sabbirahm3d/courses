eca7069e1721113984a78ada7668ad64be51ba60
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-02-20 14:15:46 -0500

    finish number 6, add helper script

--- hw/hw03/finddivisor.py
+++ hw/hw03/finddivisor.py
@@ -1,13 +1,18 @@
 from sys import argv
 
 a, b, r = [int(i) for i in argv[1:]]
-
-print(a, b, r)
 max_it = max(a, b, r)
 
-for n in xrange(max_it):
-    for m in xrange(max_it):
-        if (n * b - m * r == a):
-            raise RuntimeError("m: {0} n: {1}".format(n, m))
 
-print "None found"
+def find_divisor():
+    for n in xrange(max_it):
+        for m in xrange(max_it):
+            if (n * b - m * r == a):
+                return "{n} \cdot {b} - {m} \cdot {r}".format(
+                    n=n, b=b, m=m, r=r
+                )
+
+    return "None found"
+
+
+print find_divisor()
