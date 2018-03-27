81d85d0068557473fa3fdcf508e969f3de2c8c12
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-03-27 13:59:35 -0400

    add proper test driver

--- proj1tests/main.c
+++ proj1tests/main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -66,11 +67,8 @@ long slmbx_length_syscall(unsigned int id) {
 
 int main() {
 
-    unsigned int ptrs, prob;
-    ptrs = 16;
-    prob = 2;
-
-    printf("init: %ld\n", slmbx_init_syscall(ptrs, prob));
+    unsigned int ptrs =16;
+    unsigned int prob = 2;
 
     unsigned char *robot_buffer = (unsigned char *) "i ♥ you";
     unsigned char *human_buffer = (unsigned char *) "i love you";
@@ -81,31 +79,75 @@ int main() {
     unsigned int send_size = 10;
     unsigned int recv_size = 8;
 
-    printf("size of mailbox: %ld\n", slmbx_count_syscall(id));
+    slmbx_init_syscall(ptrs, prob);
+    printf("Initialize system (%d ptrs, %d prob) \t\terrno: %d\n",
+        ptrs, prob, errno);
+    printf("---------------------------------------------------------\n");
 
-    printf("create: %ld\n", slmbx_create_syscall(id, 1));
-    printf("send1: %ld\n", slmbx_send_syscall(id, robot_buffer, send_size));
-    printf("send2: %ld\n", slmbx_send_syscall(id, human_buffer, send_size));
-    printf("send3: %ld\n", slmbx_send_syscall(id, null_buf, send_size));
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
 
-    printf("len1: %ld ", slmbx_length_syscall(id));
-    printf("rec1: %ld ", slmbx_recv_syscall(id, good_buf, recv_size));
-    printf("msg1: %s\n", good_buf);
-    printf("size of mailbox: %ld\n", slmbx_count_syscall(id));
+    slmbx_create_syscall(id, 1);
+    printf("Create a mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
 
-    printf("len2: %ld ", slmbx_length_syscall(id));
-    printf("rec2: %ld ", slmbx_recv_syscall(id, good_buf, recv_size));
-    printf("msg2: %s\n", good_buf);
-    printf("size of mailbox: %ld\n", slmbx_count_syscall(id));
+    slmbx_send_syscall(id, robot_buffer, send_size);
+    printf("Send %d bytes of '%s' to mbx (id: %d) \terrno: %d\n",
+        send_size, robot_buffer, id, errno);
 
-    printf("len3: %ld ", slmbx_length_syscall(id));
-    printf("rec3: %ld ", slmbx_recv_syscall(id, good_buf, recv_size));
-    printf("msg3: %s\n", good_buf);
-    printf("size of mailbox: %ld\n", slmbx_count_syscall(id));
+    slmbx_send_syscall(id, human_buffer, send_size);
+    printf("Send %d bytes of '%s' to mbx (id: %d) \terrno: %d\n",
+        send_size, human_buffer, id, errno);
+    printf("---------------------------------------------------------\n");
 
-    printf("destroy: %ld\n", slmbx_shutdown_syscall());
-    printf("size of mailbox: %ld\n", slmbx_count_syscall(id));
+    slmbx_length_syscall(id);
+    printf("Length of first message in mbx (id: %d) \t\terrno: %d\n",
+        id, errno);
 
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+
+    slmbx_recv_syscall(id, good_buf, recv_size);
+    printf("Receive %d bytes of %s in mbx (id: %d) \t\terrno: %d\n",
+        recv_size, good_buf, id, errno);
+
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+    printf("---------------------------------------------------------\n");
+
+    slmbx_length_syscall(id);
+    printf("Length of first message in mbx (id: %d) \t\terrno: %d\n",
+        id, errno);
+
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+
+    slmbx_recv_syscall(id, good_buf, recv_size);
+    printf("Receive %d bytes of %s in mbx (id: %d) \t\terrno: %d\n",
+        recv_size, good_buf, id, errno);
+
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+    printf("---------------------------------------------------------\n");
+
+    slmbx_length_syscall(id);
+    printf("Length of first message in mbx (id: %d) \t\terrno: %d\n",
+        id, errno);
+
+    slmbx_recv_syscall(id, good_buf, recv_size);
+    printf("Receive %d bytes of %s in mbx (id: %d) \t\terrno: %d\n",
+        recv_size, good_buf, id, errno);
+
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+    printf("---------------------------------------------------------\n");
+
+    slmbx_shutdown_syscall();
+    printf("Destroy system \t\t\t\t\terrno: %d\n", errno);
+
+    slmbx_count_syscall(id);
+    printf("Size of mbx (id: %d) \t\t\t\terrno: %d\n", id, errno);
+
+    // free up testing buffers
     free(good_buf);
     free(null_buf);
 
