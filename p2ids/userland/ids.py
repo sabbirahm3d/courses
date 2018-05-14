a5fa7d85e827ac080fe0161e472359e3ef0fe6a4
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-05-13 22:15:31 -0400

    push final design doc

--- p2ids/userland/ids.py
+++ p2ids/userland/ids.py
@@ -29,39 +29,48 @@ class IntrusionDetectionSystem(object):
 
         for seq in vec:
             if seq["hamming_dist"] >= self.thresh:
-                print "Found sequence with hamming distance above threshold provided (>{0})".format(self.thresh)
+                print ("Found sequence with hamming distance above threshold"
+                       " provided (>{0})".format(self.thresh))
                 print seq
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description="Intrusion Detection System: Execute this script with the program you would like to trace",
+        description=("Intrusion Detection System: Execute this script "
+                     "with the programs you would like to trace"),
         add_help=False
     )
 
-    parser.add_argument(
-        "-h", "--help", action="help",
-        default=argparse.SUPPRESS,
-        help="| Show this help message and exit"
-    )
+    parser.add_argument("-h", "--help", action="help",
+                        default=argparse.SUPPRESS,
+                        help="| Show this help message and exit")
 
-    parser.add_argument("--prog", "-p", default="",
-                        metavar="cmd", help="| Program", required=True)
+    parser.add_argument("--progs", "-p", action='append', metavar="CMD",
+                        help="| List of programs", required=True)
 
-    parser.add_argument("--window", "-w", default=5, metavar="N",
+    parser.add_argument("--log", "-l", default=LOG_PATH, metavar="PATH",
+                        help="| Path to the logger executable")
+
+    parser.add_argument("--window", "-k", default=5, metavar="N",
                         type=int, help="| Window size of sequences")
+
+    parser.add_argument("--thresh", "-t", default=3, metavar="N", type=int,
+                        help=("| Minimum threshold for hamming distances"
+                              " to alarm the user"))
 
     # parse arguments to pass into function
     args = parser.parse_args()
-    log_file_name = LOG_PATH + args.prog.split()[0] + ".log"
     if (path.isfile(LOGGER_PATH)):
-        call([LOGGER_PATH, log_file_name, args.prog])
+        for prog in args.progs:
+            log_file_name = args.log + prog.split()[0] + ".log"
+            call([LOGGER_PATH, log_file_name, prog])
+            ids = IntrusionDetectionSystem(
+                prog_name=log_file_name, window_size=args.window,
+                thresh=args.thresh
+            )
+            ids.train()
+            ids.analyze(log_file_name)
+
     else:
         print "Logger executable not compiled"
-
-    ids = IntrusionDetectionSystem(
-        prog_name=log_file_name, window_size=args.window
-    )
-    ids.train()
-    ids.analyze(log_file_name)
